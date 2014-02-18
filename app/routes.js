a912763115763b5da13935e76753e16b97e67cc3
josh coffman <josh@computeristsolutions.com>
2014-02-17 19:38:49 -0700

    added a little more header config in express

--- app/routes.js
+++ app/routes.js
@@ -15,6 +15,8 @@ module.exports = function(app, passport) {
 	app.get("/*", function(req, res, next) {
 	  res.header("Access-Control-Allow-Origin", "*");
 	  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
 	  if (req.headers.host.match(/^www/) !== null) {
 	    res.redirect("http://" + req.headers.host.replace(/^www\./, "") + req.url);
 	  } else {
