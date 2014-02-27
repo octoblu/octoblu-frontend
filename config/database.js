331386340926e43c50b72eb89b9ff1ef12cf8ff0
Michael Christenson II <michael@rebelhold.com>
2014-02-26 22:06:00 -0700

    added rh for local database; added lodash

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
+// Added rh for my localbox - Michael Christenson II
 var config = {
+  rh: {
+    url: 'mongodb://localhost:27017/octoblu-development'
+  },
   development: {
-    // url: 'mongodb://localhost:27017/octoblu-development'
     url : 'mongodb://[user]:[password]@dharma.mongohq.com:10040/meshines'
   },
   test: {
-    url: 'mongodb://loclahost:27017/octoblu-test'
+    url: 'mongodb://localhost:27017/octoblu-test'
   },
   production: {
     url : 'mongodb://[user]:[password]@dharma.mongohq.com:10040/meshines'
