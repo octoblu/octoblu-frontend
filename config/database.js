ef322d7a2305486f3f6f57ba8eba51063cfdb410
Chris Matthieu <chris@matthieu.us>
2014-02-26 19:48:29 -0700

    flipped flag to prod

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 var config = {
   development: {
-    url: 'mongodb://localhost:27017/octoblu-development'
+    // url: 'mongodb://localhost:27017/octoblu-development'
+    url : 'mongodb://[user]:[password]@dharma.mongohq.com:10040/meshines'
   },
   test: {
     url: 'mongodb://loclahost:27017/octoblu-test'
