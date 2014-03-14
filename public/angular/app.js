f30db8f91f3e5c23c614b7ab430c9ce924d30feb
icereval <icereval@gmail.com>
2014-03-14 10:03:39 -0700

    more changes

--- public/angular/app.js
+++ public/angular/app.js
@@ -55,7 +55,7 @@ e2eApp.config(function($httpProvider) {
             .state('connector.devices', {
                 url: '/devices',
                 templateUrl: 'pages/connector/devices.html',
-                controller: 'DeviceController'
+                controller: 'connectorController'
             })
             .state('connector.channels', {
                 url: '/channels',
@@ -66,6 +66,11 @@ e2eApp.config(function($httpProvider) {
                 url: '/:name',
                 templateUrl: 'pages/connector/channels_detail.html',
                 controller: 'ChannelController'
+            })
+            .state('connector.gateway', {
+                url: '/gateway',
+                templateUrl: 'pages/gateway.html',
+                controller: 'gatewayController'
             })
             .state('connector.tools', {
                 url: '/tools',
@@ -168,164 +173,4 @@ e2eApp.config(function($httpProvider) {
             'http://54.203.249.138:8000/**',
             '**'
         ]);
-//
-//        $routeProvider
-//            // define SPA routes
-//            .when('/', {
-//              templateUrl : 'pages/home.html',
-//              controller  : 'mainController'
-//            })
-//            .when('/home2', {
-//              templateUrl : 'pages/home2.html',
-//              controller  : 'mainController'
-//            })
-//
-//            .when('/about', {
-//              templateUrl : 'pages/about.html',
-//              controller  : 'aboutController'
-//            })
-//
-//            .when('/contact', {
-//              templateUrl : 'pages/contact.html',
-//              controller  : 'contactController'
-//            })
-//
-//            .when('/profile', {
-//              templateUrl : 'pages/profile.html',
-//              controller  : 'profileController'
-//            })
-//
-//            .when('/dashboard', {
-//              templateUrl : 'pages/dashboard.html',
-//              controller  : 'dashboardController'
-//            })
-//
-//            .when('/connector', {
-//              templateUrl : 'pages/connector.html',
-//              controller  : 'connectorController'
-//            })
-//
-//            .when('/devices', {
-//              templateUrl : 'pages/connector.html',
-//              controller  : 'connectorController'
-//            })
-//
-//            .when('/gateways', {
-//              templateUrl : 'pages/connector.html',
-//              controller  : 'connectorController'
-//            })
-//
-//            .when('/gateway', {
-//              templateUrl : 'pages/gateway.html',
-//              controller  : 'gatewayController'
-//            })
-//
-//            .when('/apis/:name', {
-//              templateUrl : 'pages/api_detail.html',
-//              controller  : 'apiController'
-//            })
-//
-//            .when('/apis', {
-//              templateUrl : 'pages/connector.html',
-//              controller  : 'connectorController'
-//            })
-//
-//            .when('/apieditor/:name', {
-//              templateUrl : 'pages/api_editor.html',
-//              controller  : 'apieditorController'
-//            })
-//
-//            .when('/apieditor/:name/resources', {
-//              templateUrl : 'pages/api_resources.html',
-//              controller  : 'apiresourcesController'
-//            })
-//
-//            .when('/apieditor/:name/resources/:apiname', {
-//              templateUrl : 'pages/api_resource_details.html',
-//              controller  : 'apiresourcedetailController'
-//            })
-//
-//            .when('/tools', {
-//              templateUrl : 'pages/connector.html',
-//              controller  : 'connectorController'
-//            })
-//
-//            .when('/people', {
-//              templateUrl : 'pages/connector.html',
-//              controller  : 'connectorController'
-//            })
-//
-//            .when('/designer', {
-//              templateUrl : 'pages/designer.html',
-//              controller  : 'designerController'
-//            })
-//
-//            .when('/analyzer', {
-//              templateUrl : 'pages/analyzer.html',
-//              controller  : 'analyzerController'
-//            })
-//
-//            .when('/controller', {
-//              templateUrl : 'pages/controller.html',
-//              controller  : 'controllerController'
-//            })
-//
-//            .when('/community', {
-//              templateUrl : 'pages/community.html',
-//              controller  : 'communityController'
-//            })
-//
-//            .when('/community/posts/:slug', {
-//              templateUrl : 'pages/community-post.html',
-//              controller  : 'communityPostController'
-//            })
-//
-//            .when('/services', {
-//              templateUrl : 'pages/services.html',
-//              controller  : 'servicesController'
-//            })
-//
-//            .when('/admin', {
-//              templateUrl : 'pages/admin.html',
-//              controller  : 'adminController'
-//            })
-//
-//            .when('/docs', {
-//              templateUrl : 'pages/docs.html',
-//              controller  : 'docsController'
-//            })
-//
-//            .when('/pricing', {
-//              templateUrl : 'pages/pricing.html',
-//              controller  : 'pricingController'
-//            })
-//
-//            .when('/faqs', {
-//              templateUrl : 'pages/faqs.html',
-//              controller  : 'faqsController'
-//            })
-//
-//            .when('/signup', {
-//              templateUrl : 'pages/signup.html',
-//              controller  : 'signupController'
-//            })
-//
-//            .when('/login', {
-//              templateUrl : 'pages/login.html',
-//              controller  : 'loginController'
-//            })
-//
-//            .when('/forgot', {
-//              templateUrl : 'pages/forgot.html',
-//              controller  : 'forgotController'
-//            });
-
-        // .otherwise({redirectTo: '/'});
-        // .otherwise({window.location.href='/'});
-//
-//        $locationProvider
-//          .html5Mode(true)
-//          .hashPrefix('!');
-//
-//        // });
     });
