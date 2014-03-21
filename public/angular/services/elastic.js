20c5cf811442d8511c659e57d48a3def836da0f5
Chris Matthieu <chris@matthieu.us>
2014-03-21 12:59:10 -0700

    added owner to elastic query

--- public/angular/services/elastic.js
+++ public/angular/services/elastic.js
@@ -4,17 +4,39 @@ angular.module('e2eApp')
        port: '9200'
     })
     .service('elasticService', function (elasticSearchConfig, esFactory) {
-        console.log('in elastic service');
         var service = this;
         this.config = elasticSearchConfig;
         this.client = esFactory({
             host: elasticSearchConfig.host + ':' + elasticSearchConfig.port
         });
 
-        this.search = function (queryText, callback) {
+        this.search = function (queryText, ownerUuid, callback) {
+
             service.client.search({
-                q: queryText,
-                index: '_all'
+                index: '_all',
+                size: 10,
+                q: queryText + ', owner,' + ownerUuid
+
+// name:"home", owner:"5d6e9c91-820e-11e3-a399-f5b85b6b9fd0"
+// "owner" : ownerUuid
+                // "term" : { "owner" : ownerUuid }
+
+                // "term" : { "owner" : ownerUuid },
+                // // q: queryText,
+                // "query_string" : {
+                //     // "owner" : ownerUuid,
+                //     "query" : queryText
+                // }
+
+                // "query": {
+                //     "query_string": {
+                //         "query": queryText
+                //     },
+                //     "term": {
+                //         "owner" : ownerUuid
+                //     }
+                // }
+
             }, function (error, response) {
                 callback(error, response);
             });
