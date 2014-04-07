cdbe9301b69df0fd40f265b2bd8be3b210797110
Chris Matthieu <chris@matthieu.us>
2014-04-07 14:53:50 -0700

    added comments on deeplink

--- public/angular/controllers/login.js
+++ public/angular/controllers/login.js
@@ -5,10 +5,6 @@ angular.module('e2eApp')
     .controller('loginController', function($scope, $state, $location) {
         var user = $.cookie("skynetuuid");
 
-        // if($location.search().referrer){
-        //   $.cookie('redirect', $location.search().referrer);
-        // }
-
         if (user){
             $state.go('dashboard');
         }
