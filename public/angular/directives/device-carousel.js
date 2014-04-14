6f31cc9caabe69c9c241ac5520708ccacee2f402
modulus <koshin.mariano@gmail.com>
2014-04-14 09:49:10 -0700

    Merging with  origin / develop

--- public/angular/directives/device-carousel.js
+++ public/angular/directives/device-carousel.js
@@ -5,14 +5,19 @@ angular.module('e2eApp')
         return {
             restrict: 'AE',
             replace: true,
-            transclude : true,
             scope : {
                 hub : '='
 
             },
-            templateUrl: 'pages/directives/device-carousel.html',
+            template: '<div class="owl-carousel owl-theme"></div>',
             controller : 'DeviceController',
             link: function (scope, element, attr, DeviceController){
+
+                scope.$watch('hub', function(current, old){
+                     if(! scope.owl ){
+
+                     }
+                }) ;
 
 
             }
@@ -22,3 +27,51 @@ angular.module('e2eApp')
 
 
     });
+
+'use strict';
+
+//angular.module('partyfindApp')
+//    .directive('mapCarousel', function ($timeout) {
+//        return {
+//            restrict: 'E',
+//            replace: true,
+//            scope: {
+//                ngModel: '='
+//            },
+//            template:
+//                '<div class="owl-carousel owl-theme"></div>',
+//            link: function (scope, element, attrs) {
+//                scope.$watch('ngModel', function (current, old) {
+//                    if (!scope.owl) {
+//                        scope.owl = $(element);
+//
+//                        scope.owl.owlCarousel({
+//                            pagination: false,
+//                            navigation: false,
+//                            autoHeight : true
+//                        });
+//                    }
+//
+//                    _.each(scope.owl.data('owlCarousel').$userItems, function () {
+//                        scope.owl.data('owlCarousel').removeItem();
+//                    });
+//
+//                    _.each(scope.ngModel, function (model) {
+//                        scope.owl.data('owlCarousel').addItem(
+//                                '<div class="item">' +
+//                                '   <a href="' + model.data.link + '" target="_blank">' +
+//                                '       <img src="' + model.data.images.low_resolution.url + '">' +
+//                                '   </a>' +
+//                                '   <span class="caption">' +
+//                                '   <a href="' + model.data.link + '" target="_blank">' +
+//                                '       ' + model.data.user.username + ' @ ' + moment(model.created).format('LT') +
+//                                '   </a><br />' +
+//                                '       ' + model.data.caption.text +
+//                                '   </span>' +
+//                                '</div>'
+//                        );
+//                    });
+//                }, true);
+//            }
+//        };
+//    });
