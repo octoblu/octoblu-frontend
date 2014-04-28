9e812372826ad567524bb22f4977486e2286e60c
kmariano <koshin.mariano@gmail.com>
2014-04-28 08:39:19 -0700

    Added e-mail field to invitation modal. Now cancel and close button are wired up

--- public/angular/controllers/admin.js
+++ public/angular/controllers/admin.js
@@ -1,12 +1,30 @@
 'use strict';
 
 angular.module('e2eApp')
-    .controller('adminController', function($rootScope, $scope, $cookies ) {
+    .controller('adminController', function($rootScope, $scope, $cookies, $state, $modal  ) {
+
+        $scope.sendInvitation = function(){
+            var invitationModal = $modal.open({
+                templateUrl : 'pages/admin/groups/invitation.html',
+                controller : 'invitationController',
+                backdrop : true
+            });
+
+        }
 
     })
-    .controller('adminGroupController', function($rootScope, $scope, $cookies ) {
+    .controller('adminGroupDetailController', function($rootScope, $scope, $cookies ) {
 
     })
-    .controller('invitationController', function($rootScope, $scope, $cookies ) {
+    .controller('invitationController', function($rootScope, $scope, $cookies, $modalInstance ) {
+
+        $scope.send = function(){
+
+        };
+
+        $scope.close = function(){
+            $modalInstance.dismiss('cancel');
+
+        };
 
     });
