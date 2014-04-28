4de1e6449cce13cfa554ba546877aec660f0a4ea
kmariano <koshin.mariano@gmail.com>
2014-04-28 15:23:20 -0700

    added inline form instead of modal for invitations off of admin page.

--- public/angular/controllers/admin.js
+++ public/angular/controllers/admin.js
@@ -3,27 +3,22 @@
 angular.module('e2eApp')
     .controller('adminController', function($rootScope, $scope, $cookies, $state, $modal  ) {
 
-        $scope.sendInvitation = function(){
-            var invitationModal = $modal.open({
-                templateUrl : 'pages/admin/groups/invitation.html',
-                controller : 'invitationController',
-                backdrop : true
-            });
-
-        }
+//        $scope.sendInvitation = function(){
+//            var invitationModal = $modal.open({
+//                templateUrl : 'pages/admin/groups/invitation.html',
+//                controller : 'invitationController',
+//                backdrop : true
+//            });
+//
+//        }
 
     })
     .controller('adminGroupDetailController', function($rootScope, $scope, $cookies ) {
 
     })
-    .controller('invitationController', function($rootScope, $scope, $cookies, $modalInstance ) {
-
+    .controller('invitationController', function($rootScope, $scope, $cookies ) {
+        //Send the invitation
         $scope.send = function(){
-
-        };
-
-        $scope.close = function(){
-            $modalInstance.dismiss('cancel');
 
         };
 
