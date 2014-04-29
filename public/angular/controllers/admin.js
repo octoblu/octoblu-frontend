28d45599179d07d81ea3cb8a1bc15f220f57bc38
Ryan Gasparini <ryan@rxgx.com>
2014-04-28 17:39:54 -0700

    Removed modal and added mock data to scope

--- public/angular/controllers/admin.js
+++ public/angular/controllers/admin.js
@@ -1,22 +1,30 @@
 'use strict';
 
 angular.module('e2eApp')
-    .controller('adminController', function($rootScope, $scope, $cookies, $state, $modal  ) {
+    .controller('adminController', function($rootScope, $scope, $cookies, $state, mockService) {
+        // TODO: Change to getDevices
+        mockService.getUserDevices($cookies.skynetuuid, $cookies.skynettoken, function(data) {
+            if (data.devices) {
+                $scope.devices = data.devices;
+            }
+        });
 
-//        $scope.sendInvitation = function(){
-//            var invitationModal = $modal.open({
-//                templateUrl : 'pages/admin/groups/invitation.html',
-//                controller : 'invitationController',
-//                backdrop : true
-//            });
-//
-//        }
+        mockService.getUserPeople($cookies.skynetuuid, function(data) {
+            if (data.people) {
+                $scope.people = data.people;
+            }
+        });
+
+        mockService.getUserGroups($cookies.skynetuuid, function(data) {
+            if (data.groups) {
+                $scope.groups = data.groups;
+            }
+        });
+    })
+    .controller('adminGroupDetailController', function($rootScope, $scope, $cookies) {
 
     })
-    .controller('adminGroupDetailController', function($rootScope, $scope, $cookies ) {
-
-    })
-    .controller('invitationController', function($rootScope, $scope, $cookies ) {
+    .controller('invitationController', function($rootScope, $scope, $cookies) {
         //Send the invitation
         $scope.send = function(){
 
