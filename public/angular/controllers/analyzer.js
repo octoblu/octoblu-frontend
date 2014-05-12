5af808490ce21a4651a3f4f9817805edf5102808
Chris Matthieu <chris@matthieu.us>
2014-05-12 08:34:10 -0700

    updates

--- public/angular/controllers/analyzer.js
+++ public/angular/controllers/analyzer.js
@@ -52,6 +52,83 @@ angular.module('e2eApp')
               $scope.currentPage = pageNo;
             };
 
+            // SETUP CHART
+            // http://smoothiecharts.org/tutorial.html
+
+            var line1 = new TimeSeries();
+            var line2 = new TimeSeries();
+
+            // setInterval(function() {
+            //   line1.append(new Date().getTime(), Math.random()*100);
+            //   line2.append(new Date().getTime(), Math.random()*100);
+            // }, 1000);
+
+            var smoothie = new SmoothieChart({ grid: { strokeStyle: 'rgb(125, 0, 0)', fillStyle: 'rgb(60, 0, 0)', lineWidth: 1, millisPerLine: 250, verticalSections: 6 } });
+            smoothie.addTimeSeries(line1, { strokeStyle: 'rgb(0, 255, 0)', fillStyle: 'rgba(0, 255, 0, 0.4)', lineWidth: 3 });
+            smoothie.addTimeSeries(line2, { strokeStyle: 'rgb(255, 0, 255)', fillStyle: 'rgba(255, 0, 255, 0.3)', lineWidth: 3 });
+
+            smoothie.streamTo(document.getElementById("mycanvas"), 1000);
+
+            // connect to skynet
+            var skynetConfig = {
+                'uuid': $scope.skynetuuid,
+                'token': $scope.skynettoken
+            };
+
+            skynet(skynetConfig, function (e, socket) {
+                if (e) throw e;
+
+                // Get user's devices
+                ownerService.getDevices($scope.skynetuuid, $scope.skynettoken, function (data) {
+                    $scope.devices = data.devices;
+
+                   // Subscribe to user's devices messages and events
+                   if (data.devices) {
+                       _.each(data.devices, function (device) {
+                           socket.emit('subscribe', {
+                               'uuid': device.uuid,
+                               'token': device.token
+                           }, function (data) {
+                               // console.log(data);
+                           });
+
+                       });
+                   }
+
+                });
+
+                socket.on('message', function (message) {
+                  // plot data
+                  // {"payload":{"uuid":"99ede351-d6f6-11e3-abcd-1d32e7e917fb","temperature":"78","ipAddress":"70.171.192.231"},"devices":"99ede351-d6f6-11e3-abcd-1d32e7e917fb"}
+                  
+                  // remove standard data from payload
+                  var sensorData = message.payload;
+                  delete sensorData.uuid;
+                  delete sensorData.ipAddress;
+
+                  var index = 0
+                  for (var property in sensorData) {
+                      if (sensorData.hasOwnProperty(property)) {
+                        index = index + 1
+                        console.log('+' + sensorData[property].trim() + '+');
+                        if (sensorData[property] != undefined){
+                          if (index == 1){
+                            line1.append(new Date().getTime(), sensorData[property].trim()*1);  
+                          } else if (index == 2){
+                            line2.append(new Date().getTime(), sensorData[property].trim()*1);  
+                          }
+                          
+                        }
+                      }
+                  }
+
+                  // line1.append(new Date().getTime(), Math.random());
+                  // line2.append(new Date().getTime(), Math.random());
+
+
+                });
+
+            });
 
         });
     });
