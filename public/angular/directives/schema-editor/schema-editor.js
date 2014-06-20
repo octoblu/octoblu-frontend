83314592bc6045e21fe129459bf95b344c7f5504
kmariano <koshin.mariano@gmail.com>
2014-06-20 14:10:07 -0700

    The JSON schema editor will show the properties for the options

--- public/angular/directives/schema-editor/schema-editor.js
+++ public/angular/directives/schema-editor/schema-editor.js
@@ -7,15 +7,19 @@ angular.module('octobluApp')
             scope: {
                 schema: '=',
                 model: '=',
-                additionalProperties: '='
+                additionalProperties: '=',
+                allowJsonEdit : '='
             },
             link: function(scope, element, attrs) {
+                var schema = _.extend({ title : 'Options'}, scope.schema);
                 var editor = new JSONEditor(element[0],
-                    {schema: scope.schema,
+                    {schema: schema,
                         no_additional_properties: !scope.additionalProperties,
                         theme: 'bootstrap3',
                         startval: scope.model,
-                        disable_collapse: true
+                        disable_collapse: true,
+                        disable_edit_json : ! scope.allowJsonEdit
+
                     });
                 editor.on('change', function(){
                    angular.copy(editor.getValue(), scope.model);
