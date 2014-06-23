35eb465861de1e44997a24fd79a91d6761f76ea6
kmariano <koshin.mariano@gmail.com>
2014-06-22 18:33:21 -0700

    Fixed add / edit and delete devices on the subdevices. Changed host and port connections for skynet sockets.

--- public/angular/app.js
+++ public/angular/app.js
@@ -3,7 +3,7 @@
 // create the module and name it octobluApp
 angular.module('octobluApp', ['ngAnimate', 'ngSanitize', 'ngCookies', 'ui.bootstrap', 'ui.router', 'ui.utils', 'angular-google-analytics', 'elasticsearch', 'ngResource'])
     .constant('skynetConfig', {
-        'host': 'skynet.im, //change to the skynet.im instance
+        'host': 'skynet.im', //change to the skynet.im instance
         'port': '80'
     })
     .constant('reservedProperties', ['$$hashKey', '_id'])
