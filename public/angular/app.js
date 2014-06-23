89d7f10e8978684481350c7547ff9ac2df42dd4d
kmariano <koshin.mariano@gmail.com>
2014-06-23 00:19:26 -0700

    Fixing advanced messaging now the messages are sent to skynet

--- public/angular/app.js
+++ public/angular/app.js
@@ -3,8 +3,8 @@
 // create the module and name it octobluApp
 angular.module('octobluApp', ['ngAnimate', 'ngSanitize', 'ngCookies', 'ui.bootstrap', 'ui.router', 'ui.utils', 'angular-google-analytics', 'elasticsearch', 'ngResource'])
     .constant('skynetConfig', {
-        'host': '127.0.0.1', //change to the skynet.im instance
-        'port': '3000'
+        'host': 'skynet.im', //change to the skynet.im instance
+        'port': '80'
     })
     .constant('reservedProperties', ['$$hashKey', '_id'])
     // enabled CORS by removing ajax header
