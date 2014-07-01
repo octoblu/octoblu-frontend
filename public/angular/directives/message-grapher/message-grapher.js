5fd3fe004df64ab1143d5fcd092f560b1916bbb2
redaphid <aaron@loqwai.com>
2014-07-01 16:50:55 -0700

    graphing multiple properties in a message works!

--- public/angular/directives/message-grapher/message-grapher.js
+++ public/angular/directives/message-grapher/message-grapher.js
@@ -3,25 +3,47 @@ angular.module('octobluApp')
         return {
             restrict: 'AE',
             templateUrl: 'angular/directives/message-grapher/message-grapher.html',
-            replace: true,
             scope: {
                 device: '='
             },
             link: function (scope, element) {
-                var smoothie = new SmoothieChart();
-                var line1 = new TimeSeries();
-                smoothie.addTimeSeries(line1);
-                smoothie.streamTo(element.find('canvas')[0]);
-                scope.$on('skynet:message:' + scope.device.uuid, function(){
-                   console.log('directive got event');
-                    line1.append(new Date().getTime(), 1);
+                var messages = [];
+                var messageLines = {
+                    message: new TimeSeries()
+                };
+                var smoothie = new SmoothieChart({
+                    grid: { strokeStyle: 'rgb(125, 0, 0)', fillStyle: 'rgb(60, 0, 0)',
+                        lineWidth: 1, millisPerLine: 250, verticalSections: 6 },
+                    labels: { fillStyle: 'rgb(255,255,255)' }
+                });
+                smoothie.addTimeSeries(messageLines.message, { strokeStyle: 'rgb(0, 255, 0)', fillStyle: 'rgba(0, 255, 0, 0.4)', lineWidth: 3 });
+                smoothie.streamTo(element.find('canvas')[0], 200);
+                scope.$on('skynet:message:' + scope.device.uuid, function (event, message) {
+                    messages.push(message.payload);
+                    _.each(_.keys(message.payload), function (key) {
+                        if ((typeof message.payload[key] === 'number') && !messageLines[key]) {
+                            messageLines[key] = new TimeSeries();
+                            smoothie.addTimeSeries(messageLines[key], { strokeStyle: 'rgb(255,0, 0)', fillStyle: 'rgba(255,0,0,0.4)', lineWidth: 3 });
+                            console.log('added line for: ' + key);
+                        }
+                    });
                 });
 
-                var intervalPromise = $interval(function(){
-                    line1.append(new Date().getTime(), 0);
+                var intervalPromise = $interval(function () {
+                    messageLines.message.append(new Date().getTime(), messages.length);
+
+                    if (messages.length) {
+                        _.each(_.keys(messageLines), function (key) {
+                            if (key !== 'message' && (typeof messages[0][key] === 'number')) {
+                                messageLines[key].append(new Date().getTime(), messages[0][key]);
+                            }
+                        });
+                    }
+
+                    messages = [];
                 }, 1000);
 
-                scope.$on('$destroy', function(){
+                scope.$on('$destroy', function () {
                     $interval.cancel(intervalPromise);
                 });
             }
