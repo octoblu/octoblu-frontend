47f7816ecb7a28f015ca340c5149d26753059a0a
redaphid <aaron@loqwai.com>
2014-07-01 17:49:03 -0700

    Smoothie on the device detail page complete!

--- public/angular/directives/message-grapher/message-grapher.js
+++ public/angular/directives/message-grapher/message-grapher.js
@@ -7,39 +7,60 @@ angular.module('octobluApp')
                 device: '='
             },
             link: function (scope, element) {
-                var messages = [];
-                var messageLines = {
-                    message: new TimeSeries()
-                };
+                var messages = [],
+                    lineColors = [
+                        '#b58900',
+                        '#cb4b16',
+                        '#dc322f',
+                        '#d33682',
+                        '#6c71c4',
+                        '#268bd2',
+                        '#2aa198',
+                        '#859900'
+                    ],
+                    messageLines = {
+                        message: {
+                            line: new TimeSeries(),
+                            color: lineColors.pop()
+                        }
+                    };
+                scope.messageLines = messageLines;
+
                 var smoothie = new SmoothieChart({
-                    grid: { strokeStyle: 'rgb(125, 0, 0)', fillStyle: 'rgb(60, 0, 0)',
+                    grid: { strokeStyle: '#657b83', fillStyle: '#002b36',
                         lineWidth: 1, millisPerLine: 250, verticalSections: 6 },
-                    labels: { fillStyle: 'rgb(255,255,255)' }
+                    labels: { fillStyle: '#fdf6e3' }
                 });
-                smoothie.addTimeSeries(messageLines.message, { strokeStyle: 'rgb(0, 255, 0)', fillStyle: 'rgba(0, 255, 0, 0.4)', lineWidth: 3 });
-                smoothie.streamTo(element.find('canvas')[0], 200);
+                smoothie.addTimeSeries(messageLines.message.line, { strokeStyle: messageLines.message.color, lineWidth: 3 });
+                smoothie.streamTo(element.find('canvas')[0]);
                 scope.$on('skynet:message:' + scope.device.uuid, function (event, message) {
                     messages.push(message.payload);
                     _.each(_.keys(message.payload), function (key) {
                         if ((typeof message.payload[key] === 'number') && !messageLines[key]) {
-                            messageLines[key] = new TimeSeries();
-                            smoothie.addTimeSeries(messageLines[key], { strokeStyle: 'rgb(255,0, 0)', fillStyle: 'rgba(255,0,0,0.4)', lineWidth: 3 });
+                            messageLines[key] = {
+                                line: new TimeSeries(),
+                                color: lineColors.pop()
+                            };
+                            smoothie.addTimeSeries(messageLines[key].line, { strokeStyle: messageLines[key].color, lineWidth: 3 });
                             console.log('added line for: ' + key);
                         }
                     });
                 });
 
                 var intervalPromise = $interval(function () {
-                    messageLines.message.append(new Date().getTime(), messages.length);
-
+                    messageLines.message.line.append(new Date().getTime(), messages.length);
+                    console.log(messages.length);
                     if (messages.length) {
                         _.each(_.keys(messageLines), function (key) {
-                            if (key !== 'message' && (typeof messages[0][key] === 'number')) {
-                                messageLines[key].append(new Date().getTime(), messages[0][key]);
+                            if (key !== 'message') {
+                                if (typeof messages[0][key] === 'number') {
+                                    messageLines[key].line.append(new Date().getTime(), messages[0][key]);
+                                } else {
+                                    messageLines[key].line.append(new Date().getTime(), 0);
+                                }
                             }
                         });
                     }
-
                     messages = [];
                 }, 1000);
 
