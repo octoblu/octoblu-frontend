4378d53fdbfebe758b4d46d043377ebd970c3ff6
redaphid <aaron@loqwai.com>
2014-07-14 15:11:06 -0700

    wip on smoothie-resizer.

--- public/angular/directives/message-grapher/message-grapher.js
+++ public/angular/directives/message-grapher/message-grapher.js
@@ -1,42 +1,34 @@
 angular.module('octobluApp')
-    .directive('messageGrapher', function ($interval) {
+    .directive('messageGrapher', function ($interval, $window) {
         return {
             restrict: 'AE',
             templateUrl: 'angular/directives/message-grapher/message-grapher.html',
             scope: {
-                device: '='
+                device: '=',
+                width: '='
             },
             link: function (scope, element) {
                 var messages = [],
-                    lineColors = [
-                        '#b58900',
-                        '#cb4b16',
-                        '#dc322f',
-                        '#d33682',
-                        '#6c71c4',
-                        '#268bd2',
-                        '#2aa198',
-                        '#859900'
-                    ],
-                    messageLines = {
-                        message: {
-                            line: new TimeSeries(),
-                            color: lineColors.pop()
-                        }
-                    };
-                scope.messageLines = messageLines;
+                    lineColors = [],
+                    smoothie,
+                    messageLines;
 
-                var smoothie = new SmoothieChart({
-                    grid: { strokeStyle: '#657b83', fillStyle: '#002b36',
-                        lineWidth: 1, millisPerLine: 250, verticalSections: 6 },
-                    labels: { fillStyle: '#fdf6e3' }
-                });
-                smoothie.addTimeSeries(messageLines.message.line, { strokeStyle: messageLines.message.color, lineWidth: 3 });
-                smoothie.streamTo(element.find('canvas')[0]);
+                if (scope.width) {
+                    scope.graphWidth = scope.width;
+                    if (scope.width === "stretch") {
+                        scope.graphWidth = $window.innerWidth;
+                    }
+                } else {
+                    scope.graphWidth = 400;
+                }
+
+                initializeSmoothie();
                 var eventName = 'skynet:message';
+
                 if (scope.device) {
                     eventName += ':' + scope.device;
                 }
+
                 scope.$on(eventName, function (event, message) {
                     messages.push(message.payload);
                     if (scope.device) {
@@ -67,11 +59,46 @@ angular.module('octobluApp')
                         });
                     }
                     messages = [];
+
+                    if (scope.width === 'stretch' && scope.graphWidth !== $window.innerWidth) {
+                        console.log('resize smoothie');
+                    }
+
                 }, 1000);
 
                 scope.$on('$destroy', function () {
                     $interval.cancel(intervalPromise);
                 });
+
+                function initializeSmoothie() {
+                    lineColors = [
+                        '#b58900',
+                        '#cb4b16',
+                        '#dc322f',
+                        '#d33682',
+                        '#6c71c4',
+                        '#268bd2',
+                        '#2aa198',
+                        '#859900'
+                    ];
+
+                    messageLines = {
+                        message: {
+                            line: new TimeSeries(),
+                            color: lineColors.pop()
+                        }
+                    };
+
+                    smoothie = new SmoothieChart({
+                        grid: { strokeStyle: '#657b83', fillStyle: '#002b36', lineWidth: 1, millisPerLine: 250, verticalSections: 6 },
+                        labels: { fillStyle: '#fdf6e3' }
+                    });
+
+                    smoothie.addTimeSeries(messageLines.message.line, { strokeStyle: messageLines.message.color, lineWidth: 3 });
+                    smoothie.streamTo(element.find('canvas')[0]);
+                }
             }
         }
-    });
+    }
+)
+;
