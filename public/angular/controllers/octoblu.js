42d845dd572665365cd856e81f26e74b82761257
Roy van de Water <dev+roy@integrumtech.com>
2014-07-16 16:42:07 -0700

    Fix collapsible navbar

--- public/angular/controllers/octoblu.js
+++ public/angular/controllers/octoblu.js
@@ -2,6 +2,7 @@
 
 angular.module('octobluApp')
     .controller('OctobluController', function ($state, $rootScope, AuthService, currentUser, skynetService, myDevices) {
+        $rootScope.navIsCollapsed = true;
         $rootScope.currentUser = currentUser;
         $rootScope.myDevices = myDevices;
         $rootScope.logout = function () {
