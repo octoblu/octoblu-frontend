15553be23d31bc61e86842ed545fff96753f2d42
Roy van de Water <dev+roy@integrumtech.com>
2014-07-16 15:45:27 -0700

    Consistent subdevice naming

--- public/angular/controllers/add-subdevice-form.js
+++ public/angular/controllers/add-subdevice-form.js
@@ -2,16 +2,18 @@
 
 angular.module('octobluApp')
     .controller('addSubdeviceFormController', function($scope, $stateParams, NodeTypeService, PluginService, deviceService) {
-        $scope.newSubDevice = {};
+        $scope.newSubdevice = {};
 
         NodeTypeService.getNodeTypes().then(function(nodeTypes){
             $scope.nodeType = _.findWhere(nodeTypes, {_id: $stateParams.deviceId});
             return deviceService.getGateways();
         }).then(function(gateways){
             $scope.availableGateways = gateways;
-            $scope.newSubDevice.gateway = _.findWhere(gateways, {uuid: $stateParams.gatewayId});
-            return PluginService.getOrInstallPlugin($scope.newSubDevice.gateway);
+            $scope.newSubdevice.gateway = _.findWhere(gateways, {uuid: $stateParams.gatewayId});
+            return PluginService.getOrInstallPlugin($scope.newSubdevice.gateway);
         }).then(function(plugin){
-            $scope.newSubDevice.plugin = plugin;
+            console.log('Found plugin', plugin);
+            $scope.newSubdevice.plugin = plugin;
+            console.log('Ready to render:', $scope.newSubdevice);
         });
     });
