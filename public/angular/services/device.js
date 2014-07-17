67e70d1ac1b27820cf53c94e94e392fb64032cc3
redaphid <aaron@loqwai.com>
2014-07-17 10:45:00 -0700

    migrated device service to use skynet service.

--- public/angular/services/device.js
+++ public/angular/services/device.js
@@ -1,154 +1,159 @@
 angular.module('octobluApp')
-    .service('deviceService', function ($q, $http, AuthService, skynetService, reservedProperties) {
-        return AuthService.getCurrentUser()
-            .then(function (currentUser) {
+    .service('deviceService', function ($q, $http, $rootScope, skynetService, reservedProperties) {
+        var myDevices = [];
+        var skynetPromise = skynetService.getSkynetConnection();
 
-                skynetService.getSkynetConnection().then(function (skynetConnection) {
-                    var myDevices = [];
+        function addDevice(device) {
+            myDevices.push(device);
+            skynetPromise.then(function (skynetConnection) {
+                skynetConnection.unsubscribe({uuid: device.uuid, token: device.token});
+                skynetConnection.subscribe({uuid: device.uuid, token: device.token});
+            });
+        }
 
-                    function addDevice(device) {
-                        myDevices.push(device);
-                        skynetConnection.unsubscribe({uuid: device.uuid, token: device.token});
-                        skynetConnection.subscribe({uuid: device.uuid, token: device.token});
+        skynetPromise.then(function (skynetConnection) {
+            skynetConnection.on('message', function (message) {
+                $rootScope.$broadcast('skynet:message:' + message.fromUuid, message);
+                if (message.payload && _.has(message.payload, 'online')) {
+                    var device = _.findWhere(myDevices, {uuid: message.fromUuid});
+                    if (device) {
+                        device.online = message.payload.online;
                     }
+                }
+            });
+        });
 
-                    skynetConnection.on('message', function (message) {
-                        $rootScope.$broadcast('skynet:message:' + message.fromUuid, message);
-                        if (message.payload && _.has(message.payload, 'online')) {
-                            var device = _.findWhere(myDevices, {uuid: message.fromUuid});
-                            if (device) {
-                                device.online = message.payload.online;
-                            }
-                        }
-                    });
-
-                    var service = {
-                        getDevices: function (force) {
-                            var defer = $q.defer();
-                            if (myDevices && myDevices.length && !force) {
-                                defer.resolve(myDevices);
-                            } else {
-                                skynetConnection.mydevices({}, function (result) {
-                                    angular.copy([], myDevices);
-                                    _.each(result, function (device) {
-                                        addDevice(device);
-                                    });
-
-                                    defer.resolve(result);
-                                });
-                            }
-
-                            return defer.promise;
-                        },
-
-                        registerDevice: function (options) {
-                            return service.initializeDevice(options)
-                                .then(function (result) {
-                                    var device = _.extend({}, result, options);
-                                    return service.updateDevice(device);
-                                }).then(function (device) {
+        var service = {
+            getDevices: function (force) {
+                var defer = $q.defer();
+                if (myDevices.length && !force) {
+                    defer.resolve(myDevices);
+                } else {
+                    skynetPromise
+                        .then(function (skynetConnection) {
+                            skynetConnection.mydevices({}, function (result) {
+                                angular.copy([], myDevices);
+                                _.each(result, function (device) {
                                     addDevice(device);
                                 });
-                        },
-
-                        initializeDevice: function (options) {
-                            var device = _.omit(options, reservedProperties),
-                                defer = $q.defer();
-
-                            device.owner = user.skynet.uuid;
-
-                            skynetConnection.register(device, function (result) {
-                                defer.resolve(result);
-                            });
-
-                            return defer.promise;
-                        },
-
-                        claimDevice: function (options) {
-                            var device = _.omit(options, reservedProperties),
-                                defer = $q.defer();
-
-                            skynetConnection.claimdevice(device, function (data) {
-                                defer.resolve(data);
-                                options.owner = currentUser.uuid;
-                                addDevice(options);
-                            });
-
-                            return defer.promise;
-                        },
-
-                        updateDevice: function (options) {
-                            var safeDevice = _.omit(options, reservedProperties),
-                                defer = $q.defer();
-
-                            skynetConnection.update(safeDevice, function (result) {
-                                var device = _.findWhere(myDevices, {uuid: options.uuid});
-                                if (device) {
-                                    angular.copy(result, device);
-                                }
-                                defer.resolve(result);
-                            });
-
-                            return defer.promise;
-                        },
-
-                        unregisterDevice: function (options) {
-                            var defer = $q.defer();
-                            skynetConnection.unregister({
-                                uuid: options.uuid,
-                                token: options.token}, function (result) {
-
-                                var device = _.findWhere(myDevices, {uuid: options.uuid});
-                                if (device) {
-                                    myDevices.splice(_.indexOf(myDevices, device));
-                                }
 
                                 defer.resolve(result);
                             });
+                        });
+                }
+                return defer.promise;
+            },
 
-                            return defer.promise;
-                        },
+            registerDevice: function (options) {
+                return service.initializeDevice(options)
+                    .then(function (result) {
+                        var device = _.extend({}, result, options);
+                        return service.updateDevice(device);
+                    }).then(function (device) {
+                        addDevice(device);
+                    });
+            },
 
-                        getUnclaimedDevices: function () {
-                            var defer = $q.defer();
-                            skynetConnection.localdevices({}, function (result) {
-                                defer.resolve(result);
-                            });
-                            return defer.promise;
-                        },
+            initializeDevice: function (options) {
+                var device = _.omit(options, reservedProperties),
+                    defer = $q.defer();
 
-                        gatewayConfig: function (options) {
-                            var defer = $q.defer();
-                            skynetConnection.gatewayConfig(options, function (result) {
-                                defer.resolve(result);
-                            });
+                skynetPromise.then(function (skynetConnection) {
+                    device.owner = user.skynet.uuid;
 
-                            return defer.promise;
-                        },
-
-                        createSubdevice: function (options) {
-                            return service.gatewayConfig(_.extend({ method: 'createSubdevice' },
-                                _.omit(options, reservedProperties)))
-                                .then(function(result){
-                                    var device = _.findWhere(myDevices, {uuid: options.uuid});
-                                    if(device){
-                                        device.subdevices.push(_.pick(options, 'name', 'options', 'type'));
-                                    }
-                                });
-                        },
-
-                        updateSubdevice: function (options) {
-                            return service.gatewayConfig(_.extend({ method: 'updateSubdevice' },
-                                _.omit(options, reservedProperties)))
-                        },
-
-                        deleteSubdevice: function (options) {
-                            return service.gatewayConfig(_.extend({ method: 'deleteSubdevice' },
-                                _.omit(options, reservedProperties)));
-                        }
-                    };
-
-                    return service;
+                    skynetConnection.register(device, function (result) {
+                        console.log('registered device!');
+                        defer.resolve(result);
+                    });
                 });
-            });
+                return defer.promise;
+            },
+
+            claimDevice: function (options) {
+                var device = _.omit(options, reservedProperties),
+                    defer = $q.defer();
+
+                skynetPromise.then(function (skynetConnection) {
+                    skynetConnection.claimdevice(device, function (data) {
+                        defer.resolve(data);
+                        addDevice(options);
+                    });
+                });
+
+                return defer.promise;
+            },
+
+            updateDevice: function (options) {
+                var device = _.omit(options, reservedProperties),
+                    defer = $q.defer();
+
+                skynetPromise.then(function (skynetConnection) {
+                    skynetConnection.update(device, function (result) {
+                        angular.copy(result, options);
+                        defer.resolve(result);
+                    });
+                });
+
+                return defer.promise;
+            },
+
+            unregisterDevice: function (options) {
+                var device = _.findWhere(myDevices, {uuid: options.uuid}),
+                    defer = $q.defer();
+
+                skynetPromise.then(function (skynetConnection) {
+                    skynetConnection.unregister({
+                        uuid: options.uuid,
+                        token: options.token}, function (result) {
+
+                        if (device) {
+                            myDevices.splice(_.indexOf(myDevices, device));
+                        }
+
+                        defer.resolve(result);
+                    });
+                });
+
+                return defer.promise;
+            },
+
+            getUnclaimedDevices: function () {
+                var defer = $q.defer();
+
+                skynetPromise.then(function (skynetConnection) {
+                    skynetConnection.localdevices({}, function (result) {
+                        defer.resolve(result);
+                    });
+                });
+
+                return defer.promise;
+            },
+
+            gatewayConfig: function (options) {
+                var defer = $q.defer();
+
+                skynetPromise.then(function (skynetConnection) {
+                    skynetConnection.gatewayConfig(options, function (result) {
+                        defer.resolve(result);
+                    });
+                });
+
+                return defer.promise;
+            },
+
+            createSubdevice: function (options) {
+                return service.gatewayConfig(_.extend({ method: 'createSubdevice' },
+                    _.omit(options, reservedProperties)));
+            },
+
+            updateSubdevice: function (options) {
+                return service.gatewayConfig(_.extend({ method: 'updateSubdevice' },
+                    _.omit(options, reservedProperties)));
+            },
+
+            deleteSubdevice: function (options) {
+                return service.gatewayConfig(_.extend({ method: 'deleteSubdevice' },
+                    _.omit(options, reservedProperties)));
+            }
+        };
     });
--- public/angular/services/skynet.js
+++ public/angular/services/skynet.js
@@ -1,12 +1,8 @@
 angular.module('octobluApp')
     .service('skynetService', function ($q, $rootScope, skynetConfig, AuthService) {
-        var skynetConnection,
-            user,
-            defer = $q.defer(),
-            skynetPromise = defer.promise;
+        var skynetConnection, user, defer = $q.defer(), skynetPromise = defer.promise;
 
         AuthService.getCurrentUser().then(function (currentUser) {
-
 
             user = currentUser;
             var conn = skynet.createConnection({
@@ -37,177 +33,18 @@ angular.module('octobluApp')
             });
         });
 
-        var service = {
-            /**
-             * gets the skynetConnection. This is so that when we migrate the
-             * device centric apis to the device service, the device service
-             * can grab the skynetConnection and make the underlying api calls.
-             * @returns {Deferred.promise|*}
-             */
+        return {
+
             getSkynetConnection: function () {
-                var defer = $q.defer(), promise = defer.promise;
+                var defer = $q.defer();
 
                 skynetPromise.then(function () {
                     return skynetConnection;
                 });
-                return promise;
+
+                return defer.promise();
             },
 
-            /**
-             *
-             * @param options
-             * @returns {Deferred.promise|*}
-             */
-            gatewayConfig: function (options) {
-                var defer = $q.defer(), promise = defer.promise;
-
-                skynetPromise.then(function () {
-                    skynetConnection.gatewayConfig(options, function (result) {
-                        console.log('got gateway configuration!');
-                        defer.resolve(result);
-                    });
-                });
-
-                return promise;
-            },
-
-            /**
-             *
-             * @param options
-             * @returns {Deferred.promise|*}
-             */
-            claimDevice: function (options) {
-                var device = _.omit(options, reservedProperties),
-                    defer = $q.defer(), promise = defer.promise;
-
-                skynetPromise.then(function () {
-                    skynetConnection.claimdevice(device, function (data) {
-                        console.log('claim device results: ');
-                        console.log(data);
-                        defer.resolve(data);
-                    });
-                });
-
-                return promise;
-            },
-
-            claimAndUpdateDevice: function (options) {
-                var device = _.omit(options, reservedProperties);
-
-                return skynetPromise.then(function () {
-                    device.owner = user.skynet.uuid;
-                    return service.updateDevice(device);
-                })
-                    .then(function () {
-                        $rootScope.myDevices.push(device);
-                    });
-            },
-
-            /**
-             *
-             * @param options
-             * @returns {Deferred.promise|*}
-             */
-            updateDevice: function (options) {
-                var device = _.omit(options, reservedProperties),
-                    defer = $q.defer(), promise = defer.promise;
-
-
-                skynetPromise.then(function () {
-                    skynetConnection.update(device, function (result) {
-                        console.log('updated device!');
-                        defer.resolve(result);
-                    });
-                });
-
-                return promise;
-            },
-
-            registerDevice: function (options) {
-                var device;
-                return service.initializeDevice(options).then(function (result) {
-                    device = _.extend({}, result, options);
-                    return service.updateDevice(device);
-                }).then(function () {
-                    $rootScope.myDevices.push(device);
-                });
-            },
-
-            /**
-             *
-             * @param options
-             * @returns {Deferred.promise|*}
-             */
-            initializeDevice: function (options) {
-                var device = _.omit(options, reservedProperties),
-                    defer = $q.defer(), promise = defer.promise;
-
-
-                skynetPromise.then(function () {
-                    device.owner = user.skynet.uuid;
-
-                    skynetConnection.register(device, function (result) {
-                        console.log('registered device!');
-                        defer.resolve(result);
-                    });
-                });
-                return promise;
-            },
-
-            /**
-             *
-             * @param options
-             * @returns {Deferred.promise|*}
-             */
-            unregisterDevice: function (options) {
-                var device = _.omit(options, reservedProperties),
-                    defer = $q.defer(), promise = defer.promise;
-
-                skynetPromise.then(function () {
-                    skynetConnection.unregister(device, function (result) {
-                        console.log('registered device!');
-                        defer.resolve(result);
-                    });
-                });
-
-                return promise;
-            },
-
-            /**
-             *
-             * @param options
-             * @returns {Deferred.promise|*}
-             */
-            createSubdevice: function (options) {
-                return service.gatewayConfig(_.extend({ method: 'createSubdevice' },
-                    _.omit(options, reservedProperties)));
-            },
-
-            /**
-             *
-             * @param options
-             * @returns {Deferred.promise|*}
-             */
-            updateSubdevice: function (options) {
-                return service.gatewayConfig(_.extend({ method: 'updateSubdevice' },
-                    _.omit(options, reservedProperties)));
-            },
-
-            /**
-             *
-             * @param options
-             * @returns {Deferred.promise|*}
-             */
-            deleteSubdevice: function (options) {
-                return service.gatewayConfig(_.extend({ method: 'deleteSubdevice' },
-                    _.omit(options, reservedProperties)));
-            },
-
-            /**
-             *
-             * @param options
-             * @returns {*}
-             */
             sendMessage: function (options) {
                 return skynetPromise.then(function () {
                     skynetConnection.message(options, function (result) {
@@ -217,7 +54,5 @@ angular.module('octobluApp')
                 });
             }
         };
-
-        return service;
     })
 ;
