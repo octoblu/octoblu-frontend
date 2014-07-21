c4a561476daadd945377ce3a8d5380e4533f6527
redaphid <aaron@loqwai.com>
2014-07-21 15:42:36 -0700

    updated my branch to latest master

--- public/angular/services/skynet.js
+++ public/angular/services/skynet.js
@@ -1,6 +1,9 @@
 angular.module('octobluApp')
-    .service('skynetService', function ($q, $rootScope, skynetConfig, AuthService) {
-        var user, defer = $q.defer(), skynetPromise = defer.promise;
+    .service('skynetService', function ($q, $rootScope, skynetConfig, AuthService, reservedProperties) {
+        var skynetConnection,
+            user,
+            defer = $q.defer(),
+            skynetPromise = defer.promise;
 
         AuthService.getCurrentUser().then(function (currentUser) {
 
@@ -13,22 +16,39 @@ angular.module('octobluApp')
             });
 
             conn.on('ready', function (data) {
+                skynetConnection = conn;
                 console.log(data);
                 console.log('Connected to skynet');
-                defer.resolve(conn);
+                defer.resolve();
             });
 
             conn.on('notReady', function (error) {
-                console.log('Skynet Error during connect', error);
+                console.log('Skynet Error during connect');
                 defer.reject(error);
             });
 
-            return skynetPromise;
+            return defer.promise;
         });
 
-        skynetPromise.then(function (skynetConnection) {
-            skynetConnection.on('message', function (message) {
-                $rootScope.$broadcast('skynet:message', message);
+        skynetPromise.then(function () {
+            console.log('registering for messages');
+            skynetConnection.on('message', processMessage);
+
+            _.each($rootScope.myDevices, function (device) {
+                console.log('Subscribing for device :' + device.uuid);
+                skynetConnection.subscribe({uuid: device.uuid, token: device.token});
+            });
+
+            $rootScope.$watch('myDevices', function (myDevices, prevMyDevices) {
+                _.each(prevMyDevices, function (device) {
+                    console.log('Unsubscribing for device :' + device.uuid);
+                    skynetConnection.unsubscribe({uuid: device.uuid});
+                });
+
+                _.each(myDevices, function (device) {
+                    console.log('Subscribing for device :' + device.uuid);
+                    skynetConnection.subscribe({uuid: device.uuid, token: device.token});
+                });
             });
         });
 
@@ -43,28 +63,37 @@ angular.module('octobluApp')
             }
         }
 
-        return {
+        var service = {
+            /**
+             * gets the skynetConnection. This is so that when we migrate the
+             * device centric apis to the device service, the device service
+             * can grab the skynetConnection and make the underlying api calls.
+             * @returns {Deferred.promise|*}
+             */
             getSkynetConnection: function () {
-                return skynetPromise.then(function (skynetConnection) {
+                var defer = $q.defer(), promise = defer.promise;
+
+                skynetPromise.then(function () {
                     return skynetConnection;
                 });
-
+                return promise;
             },
 
+            /**
+             *
+             * @param options
+             * @returns {*}
+             */
             sendMessage: function (options) {
-                return skynetPromise.then(function (skynetConnection) {
-
-                    var defer = $q.defer(), promise = defer.promise;
-
-                    skynetPromise.then(function () {
-                        console.log('sending message.');
-                        skynetConnection.message(options, function (result) {
-                            console.log('meshblu response:', result);
-                            defer.resolve(result);
-                        });
+                return skynetPromise.then(function () {
+                    skynetConnection.message(options, function (result) {
+                        console.log('sending skynet message!');
+                        return result;
                     });
-                    return promise;
                 });
             }
-        }
-    });
+        };
+
+        return service;
+    })
+;
