3a19b8ecebad6545238773279c7df028f4d3b5fd
Roy van de Water <dev+roy@integrumtech.com>
2014-07-21 16:04:25 -0700

    Add device token to details page

--- public/angular/app.js
+++ public/angular/app.js
@@ -5,8 +5,8 @@ angular.module('octobluApp', ['ngAnimate', 'ngSanitize', 'ngCookies', 'ui.bootst
     .constant('skynetConfig', {
         'host': 'wss://skynet.im',
         'port': '443'
-        // 'host': 'localhost', //change to the skynet.im instance
-        // 'port': '3000'
+        // 'host': 'wss://localhost', //change to the skynet.im instance
+        // 'port': '3001'
     })
     .constant('reservedProperties', ['$$hashKey', '_id'])
     // enabled CORS by removing ajax header
