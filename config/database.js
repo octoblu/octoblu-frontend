7240b93823696d9f29f06a1c596999ffcfb0d462
Alacer <alacer.cogitatus@gmail.com>
2014-07-22 19:11:19 -0400

    Updating to latest codes

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 var config = {
     development: {
         // url : 'mongodb://[user]:[password]@dharma.mongohq.com:10040/meshines'
-        url : process.env.OCTOBLU_DB || 'mongodb://54.186.148.255:27017/meshines',
-        skynetUrl: process.env.SKYNET_DB || 'mongodb://54.186.148.255:27017/skynet',
+        url : process.env.OCTOBLU_DB || 'mongodb://localhost:27017/meshines',
+        skynetUrl: process.env.SKYNET_DB || 'mongodb://localhost:27017/skynet',
         redisSessionUrl: process.env.REDIS_SESSION_URI || 'redis://localhost'
     },
     test: {
@@ -12,8 +12,8 @@ var config = {
     },
     production: {
         // url : 'mongodb://[user]:[password]@dharma.mongohq.com:10040/meshines'
-        url : 'mongodb://54.186.148.255:27017/meshines',
-        skynetUrl: 'mongodb://54.186.148.255:27017/skynet',
+        url : 'mongodb://localhost:27017/meshines',
+        skynetUrl: 'mongodb://localhost:27017/skynet',
         redisSessionUrl: 'redis://meshblu-redis.csy8op.0001.usw2.cache.amazonaws.com'
     }
 };
