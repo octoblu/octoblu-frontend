39437cc14846a959be47873e2bdd1d0ee908ea4c
Roy van de Water <dev+roy@integrumtech.com>
2014-07-23 08:57:30 -0700

    Don't point at localhost

--- public/angular/app.js
+++ public/angular/app.js
@@ -3,10 +3,10 @@
 // create the module and name it octobluApp
 angular.module('octobluApp', ['ngAnimate', 'ngSanitize', 'ngCookies', 'ui.bootstrap', 'ui.router', 'ui.utils', 'angular-google-analytics', 'elasticsearch', 'ngResource', 'ngTable'])
     .constant('skynetConfig', {
-        // 'host': 'wss://skynet.im',
-        // 'port': '443'
-        'host': 'ws://localhost', //change to the skynet.im instance
-        'port': '3000'
+        'host': 'wss://skynet.im',
+        'port': '443'
+        // 'host': 'ws://localhost', //change to the skynet.im instance
+        // 'port': '3000'
     })
     .constant('reservedProperties', ['$$hashKey', '_id'])
     // enabled CORS by removing ajax header
