bb792d0d5a0a9acc94736c7b776a1893a55c0248
josh coffman <josh@computeristsolutions.com>
2014-07-25 15:39:55 -0700

    added curvery monkey script

---
+++ setup/migrations/update_surveymonkey.js
@@ -0,0 +1,30 @@
+function update(api) {
+	delete api.owner;
+	api.oauth = {
+			isManual: true,
+			version: "2.0",
+			clientId: "vyscb5uzhypva3t8x2fd93hu",
+			secret: "MfS2A9Z9uBZVHrmMKY9R4ZQFUVGESAum",
+			baseURL: "https://api.surveymonkey.net",
+			accessTokenURL: "https://api.surveymonkey.net/oauth/token",
+			authTokenURL: "https://api.surveymonkey.com/oauth/authorize",
+			tokenMethod: "apikey_app_query",
+			authTokenPath: "/oauth/authorize",
+			accessTokenIncludeClientInfo: true,
+			grant_type: "authorization_code",
+			scope: "",
+			protocol: "https",
+			host: "api.surveymonkey.net",
+			passTokenInQuery: true,
+			tokenQueryParam: "oauth2_access_token",
+			auth_use_client_id_value: "rebootd",
+			auth_use_api_key: true
+		};
+	
+	db.apis.save(api);
+}
+
+var api = db.apis.findOne({name: 'Survey Monkey'});
+if(api) {
+	update(api);
+}
