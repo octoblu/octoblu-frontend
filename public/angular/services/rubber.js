a820ec5433ef2da27c63ad65809064db8928ca64
Roy van de Water <dev+roy@integrumtech.com>
2014-07-25 08:29:55 -0700

    Add buildDevices

--- public/angular/services/rubber.js
+++ public/angular/services/rubber.js
@@ -1,3 +1,8 @@
 angular.module('octobluApp')
 .service('RubberService', function () {
+  this.buildDevices = function(devices){
+    return _.map(devices, function(device){
+      return 'uuid='+device.uuid;
+    }).join(' OR ');
+  };
 });
--- test/angular/services/rubber-spec.js
+++ test/angular/services/rubber-spec.js
@@ -3,6 +3,7 @@ describe('RubberService', function () {
     var _this = this;
 
     module('octobluApp');
+
     inject(function(RubberService){
       _this.sut = RubberService;
     });
@@ -10,5 +11,33 @@ describe('RubberService', function () {
 
   it('should instantiate', function () {
     expect(this.sut).to.exist
+  });
+
+  describe('->buildDevices', function () {
+    describe('with nothing passed in', function () {
+      it('should return an empty string', function () {
+        expect(this.sut.buildDevices()).to.equal("");
+      });
+    });
+
+    describe('with an empty array of devices', function () {
+      it('should return an empty string', function () {
+        expect(this.sut.buildDevices([])).to.equal("");
+      });
+    });
+
+    describe('with an array containing a single device', function () {
+      it('should return "uuid=the_uuid"', function () {
+        var devices = [{uuid: 'the_uuid'}];
+        expect(this.sut.buildDevices(devices)).to.equal("uuid=the_uuid");
+      });
+    });
+
+    describe('with an array containing two devices', function () {
+      it('should return "uuid=the_uuid OR uuid=the_other_uuid"', function () {
+        var devices = [{uuid: 'the_uuid'}, {uuid: 'the_other_uuid'}];
+        expect(this.sut.buildDevices(devices)).to.equal("uuid=the_uuid OR uuid=the_other_uuid");
+      });
+    });
   });
 });
