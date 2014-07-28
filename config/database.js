9622f988f019a9b2290272992558c1c186556e27
Jade Meskill and Roy van de Water <dev+jade+roy@integrumtech.com>
2014-07-28 09:10:30 -0700

    Fix syntax error in database.js

--- config/database.js
+++ config/database.js
@@ -14,7 +14,7 @@ var config = {
         url : 'mongodb://172.31.44.170:27017/meshines',
         skynetUrl: 'mongodb://172.31.44.146:27017/skynet',
         redisSessionUrl: 'redis://meshblu-redis.csy8op.0001.usw2.cache.amazonaws.com'
-    }
+    },
     production: {
         // url : 'mongodb://[user]:[password]@dharma.mongohq.com:10040/meshines'
         url : 'mongodb://172.31.44.170:27017/meshines',
