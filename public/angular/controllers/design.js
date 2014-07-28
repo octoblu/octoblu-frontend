8b9fe4961c63abd47a69de6ca33c8f9fca9568ed
Chris Matthieu <chris@matthieu.us>
2014-07-27 19:27:40 -0700

    rolled back to iframe

--- public/angular/controllers/design.js
+++ public/angular/controllers/design.js
@@ -1,24 +1,55 @@
+// 'use strict';
+
+// angular.module('octobluApp')
+//     .controller('designController', function ($rootScope, $scope, $http, $injector, $location, nodeRedService, currentUser) {
+//         var getSessionFlow = function () {
+//             $http({method: 'GET', url: '/api/get/flow'})
+//                 .success(function (data, status, headers, config) {
+//                     console.log('/api/get/flow', data);
+//                     if (data.flow) {
+//                         RED.view.importFromCommunity(data.flow);
+//                     }
+//                 });
+//         };
+
+//         nodeRedService.getPort(currentUser.skynet.uuid, currentUser.skynet.token, function (port) {
+//             initializeRED();
+//             RED.wsConnect(RED.loadSettings, currentUser.skynet.uuid, currentUser.skynet.token, port);
+//             getSessionFlow();
+//         });
+
+//         $scope.save = function(){
+//             RED.save();
+//         };
+//     });
+
 'use strict';
 
 angular.module('octobluApp')
-    .controller('designController', function ($rootScope, $scope, $http, $injector, $location, nodeRedService, currentUser) {
-        var getSessionFlow = function () {
+    .controller('designController', function ($rootScope, $scope, $http, $injector, $location, nodeRedService) {
+        $scope.getSessionFlow = function () {
             $http({method: 'GET', url: '/api/get/flow'})
                 .success(function (data, status, headers, config) {
-                    console.log('/api/get/flow', data);
+                    console.log(data);
                     if (data.flow) {
-                        RED.view.importFromCommunity(data.flow);
+                        var win = angular.element('#designerFrame')[0];
+                        win.onload = function () {
+                            win.contentWindow.postMessage(data.flow, $scope.redFrame);
+                        };
                     }
                 });
         };
 
-        nodeRedService.getPort(currentUser.skynet.uuid, currentUser.skynet.token, function (port) {
-            initializeRED();
-            RED.wsConnect(RED.loadSettings, currentUser.skynet.uuid, currentUser.skynet.token, port);
-            getSessionFlow();
-        });
+        // Get NodeRed port number
+        nodeRedService.getPort($scope.currentUser.skynet.uuid, $scope.currentUser.skynet.token, function (data) {
+            $scope.redPort = data.replace(/["']/g, "");
+            $scope.redFrame = "https://" + $scope.currentUser.skynet.uuid + ":" + $scope.currentUser.skynet.token + "@designer.octoblu.com:" + $scope.redPort;
 
-        $scope.save = function(){
-            RED.save();
-        };
+            $scope.getSessionFlow();
+
+            $scope.designerFrame = {
+                skynetid: $scope.currentUser.skynet.uuid,
+                skynettoken: $scope.currentUser.skynet.token
+            };
+        });
     });
