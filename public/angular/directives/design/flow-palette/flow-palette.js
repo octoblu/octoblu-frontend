0c2385dd4655cf0d21dcb7ee4ba2f97589f55331
redaphid <aaron@loqwai.com>
2014-08-01 14:40:31 -0700

    Moving workspace into directive. Readding features one at a time.

---
+++ public/angular/directives/design/flow-palette/flow-palette.js
@@ -0,0 +1,22 @@
+angular.module('octobluApp')
+    .directive('flowPalette', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'angular/directives/design/flow-palette/flow-palette.html',
+            replace: true,
+            scope: {
+                flowNodeTypes: '='
+            },
+            controller: function ($scope) {
+                var flowNodeTypeTemplateUrl = '/angular/directives/design/flow-palette/flow-node-templates/';
+                $scope.$watch('flowNodeTypes', function () {
+                    $scope.typesByCategory = _.groupBy($scope.flowNodeTypes, 'category');
+                });
+
+                $scope.getFlowNodeTypeTemplateUrl = function (flowNodeType) {
+                    return flowNodeTypeTemplateUrl + 'default.html';
+//                    return flowNodeTypeTemplateUrl + flowNodeType.name + '.html';
+                };
+            }
+        };
+    });
