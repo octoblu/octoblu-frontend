2f4ba4455b3f683766b9bc13e6a95ebc913a6450
redaphid <aaron@loqwai.com>
2014-07-31 21:14:14 -0700

    getting node types using a "service"

--- public/angular/services/flow-node-type.js
+++ public/angular/services/flow-node-type.js
@@ -1,46 +1,11 @@
 angular.module('octobluApp')
-    .service('FlowNodeTypeService', function ($http, $q) {
-        var myFlowNodeTypes = [
-            {
-                "name": "delay",
-                "category": "function",
-                "inputs": 1,
-                "outputs": 1,
-                "ui": {
-                    "icon": "timer.png",
-                    "color": "#E6E0F8"
-                },
-                "schema": {
-                    "type": "object",
-                    "properties": {
-                        "pauseType": {
-                            "type": "string",
-                            "required": true
-                        },
-                        "timeout": {
-                            "type": "integer",
-                            "minimum": "1",
-                            "required": true
-                        },
-                        "timeoutUnits": {
-                            "type": "string",
-                            "default:": "seconds"
-                        },
-                        "rate": {
-                            "type": "integer",
-                            "minimum": "1",
-                            "required": true
-                        }
-                    }
-                }
-            }
-        ];
-
+    .service('FlowNodeTypeService', function ($http) {
         return {
             getFlowNodeTypes: function () {
-                var defer = $q.defer();
-                defer.resolve(myFlowNodeTypes);
-                return defer.promise;
+               return $http.get('/api/flow_node_types', {cache: true})
+                   .then(function(res){
+                       return res.data;
+                   });
             }
         };
     });
