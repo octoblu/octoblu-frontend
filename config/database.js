c1d3ed9d9effe6b769d89d31001f216953afc39d
Kyle Smith <kylesmith@kyleasmith.info>
2014-08-02 16:08:54 -0400

    Running Auto Merge and Update

--- config/database.js
+++ config/database.js
@@ -1,24 +1,24 @@
 var config = {
     development: {
-        url : process.env.OCTOBLU_DB || 'mongodb://localhost:27019/meshines',
-        skynetUrl: process.env.SKYNET_DB || 'mongodb://localhost:27019/skynet',
+        url : process.env.OCTOBLU_DB || 'mongodb://localhost:27017/meshines',
+        skynetUrl: process.env.SKYNET_DB || 'mongodb://localhost:27017/skynet',
         redisSessionUrl: process.env.REDIS_SESSION_URI || 'redis://localhost'
     },
     test: {
-        url: 'mongodb://localhost:27019/meshines',
-        skynetUrl: 'mongodb://localhost:27019/skynet-test',
+        url: 'mongodb://localhost:27017/meshines',
+        skynetUrl: 'mongodb://localhost:27017/skynet-test',
         redisSessionUrl: 'redis://localhost/test-octoblu-session'
     },
     staging: {
         // url : 'mongodb://[user]:[password]@dharma.mongohq.com:10040/meshines'
-        url : 'mongodb://localhost:27019/meshines',
-        skynetUrl: 'mongodb://localhost:27019/skynet',
+        url : 'mongodb://172.31.44.170:27017/meshines',
+        skynetUrl: 'mongodb://172.31.44.146:27017/skynet',
         redisSessionUrl: 'redis://meshblu-redis.csy8op.0001.usw2.cache.amazonaws.com'
     },
     production: {
         // url : 'mongodb://[user]:[password]@dharma.mongohq.com:10040/meshines'
-        url : 'mongodb://localhost:27019/meshines',
-        skynetUrl: 'mongodb://localhost:27019/skynet',
+        url : 'mongodb://172.31.44.170:27017/meshines',
+        skynetUrl: 'mongodb://172.31.44.146:27017/skynet',
         redisSessionUrl: 'redis://meshblu-redis.csy8op.0001.usw2.cache.amazonaws.com'
     }
 };
