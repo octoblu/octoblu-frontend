2e424d0efb064d3004a4b147b5948e6ee6d8c451
redaphid <aaron@loqwai.com>
2014-08-03 10:48:37 -0700

    Moved svg template to use absolute sizing - svg is relative anyway, and this allows it to work in both contexts.

--- public/angular/directives/flow/flow-node/flow-node2.js
+++ public/angular/directives/flow/flow-node/flow-node2.js
@@ -2,11 +2,6 @@ angular.module('octobluApp')
     .directive('flowNode', function () {
         return {
             restrict: 'E',
-            templateUrl: '/angular/directives/flow/flow-node-palette/flow-node-palette.html',
-            replace: true,
-            scope: {
-                flowNodeTypes: '='
-            },
             controller: function ($scope) {
 
             }
