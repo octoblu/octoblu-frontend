b5f9de69144809bd87c48dc57d851c8c11e4affd
Roy van de Water <dev+roy@integrumtech.com>
2014-08-04 09:13:28 -0700

    Add plumber for better gulp watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var   gulp = require('gulp'),
     concat = require('gulp-concat'),
       less = require('gulp-less'),
+   plumber = require('gulp-plumber'),
 sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('less:compile', function(){
   gulp.src('./assets/less/manifest.less')
+    .pipe(plumber())
     .pipe(less())
     .pipe(concat('styles.css'))
     .pipe(gulp.dest('./public/assets/stylesheets/dist/'));
@@ -12,6 +14,7 @@ gulp.task('less:compile', function(){
 
 gulp.task('javascript:concat', function(){
   gulp.src(['./public/angular/app.js', './public/angular/**/*.js'])
+    .pipe(plumber())
     .pipe(sourcemaps.init())
       .pipe(concat('application.js'))
     .pipe(sourcemaps.write('.'))
