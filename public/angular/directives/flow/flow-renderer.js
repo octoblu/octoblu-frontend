8fd415b800f6c3325dd70bee3861b664c7c0cbb9
redaphid <aaron@loqwai.com>
2014-08-05 15:18:44 -0700

    Simplified flow link renderer

--- public/angular/directives/flow/flow-renderer.js
+++ public/angular/directives/flow/flow-renderer.js
@@ -2,8 +2,7 @@ angular.module('octobluApp')
   .service('FlowRenderer', function (FlowNodeRenderer, FlowLinkRenderer) {
     return function (renderScope) {
       var dispatch = d3.dispatch('flowChanged', 'nodeSelected');
-      var linkRenderer = FlowLinkRenderer(renderScope),
-        nodeRenderer = FlowNodeRenderer(renderScope);
+      var nodeRenderer = FlowNodeRenderer(renderScope);
 
       renderScope.on('click', function () {
         if (d3.event.defaultPrevented) {
@@ -12,28 +11,28 @@ angular.module('octobluApp')
         dispatch.nodeSelected(null);
       });
 
-      function getNodeLinks(nodeId, links) {
-        var fromLinks = _.where(links, { 'from': nodeId });
-        var toLinks = _.where(links, { 'to': nodeId });
-
-        return fromLinks.concat(toLinks);
+      function renderLinks(flow) {
+        renderScope.selectAll('.flow-link').remove();
+        _.each(flow.links, function(link){
+          FlowLinkRenderer.render(renderScope, link, flow.nodes);
+        });
       }
 
       return {
         render: function (flow) {
           nodeRenderer.render(flow.nodes);
-          linkRenderer.add(flow.links);
+          renderLinks(flow);
 
           nodeRenderer
             .on('nodeMoved', function (flowNode) {
-              var nodeLinks = getNodeLinks(flowNode.id, flow.links);
-              linkRenderer.render(nodeLinks);
+              renderLinks(flow);
             });
 
           nodeRenderer
             .on('nodeChanged', function (flowNode) {
               dispatch.flowChanged(flow);
             });
+
           nodeRenderer
             .on('nodeClicked', function (flowNode) {
               dispatch.nodeSelected(flowNode);
@@ -41,7 +40,6 @@ angular.module('octobluApp')
         },
         clear: function () {
           nodeRenderer.clear();
-          linkRenderer.clear();
         },
         on: function (event, callback) {
           return dispatch.on(event, callback);
