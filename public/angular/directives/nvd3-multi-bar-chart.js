9f2b8e3f63b450bf1f6d2a2537c067d471ebd1fd
Kyle Smith <kylesmith@kyleasmith.info>
2014-08-05 19:36:01 -0400

    Working Bar Graph and Line with Focus Graph

--- public/angular/directives/nvd3-multi-bar-chart.js
+++ public/angular/directives/nvd3-multi-bar-chart.js
@@ -1,7 +1,50 @@
 'use strict';
 
+/* Inspired by Lee Byron's test data generator. */
+function stream_layers(n, m, o) {
+  if (arguments.length < 3) o = 0;
+  function bump(a) {
+    var x = 1 / (.1 + Math.random()),
+        y = 2 * Math.random() - .5,
+        z = 10 / (.1 + Math.random());
+    for (var i = 0; i < m; i++) {
+      var w = (i / m - y) * z;
+      a[i] += x * Math.exp(-w * w);
+    }
+  }
+  return d3.range(n).map(function() {
+      var a = [], i;
+      for (i = 0; i < m; i++) a[i] = o + o * Math.random();
+      for (i = 0; i < 5; i++) bump(a);
+      return a.map(stream_index);
+    });
+}
+
+/* Another layer generator using gamma distributions. */
+function stream_waves(n, m) {
+  return d3.range(n).map(function(i) {
+    return d3.range(m).map(function(j) {
+        var x = 20 * j / m - i / 3;
+        return 2 * x * Math.exp(-.5 * x);
+      }).map(stream_index);
+    });
+}
+
+function stream_index(d, i) {
+  return {x: i, y: Math.max(0, d)};
+}
+
+function exampleData() {
+  return stream_layers(3,10+Math.random()*100,.1).map(function(data, i) {
+    return {
+      key: 'Stream #' + i,
+      values: data
+    };
+  });
+}
+
 angular.module('octobluApp')
-    .directive('nvd3LineWithFocusChart', function () {
+    .directive('nvd3MultibarChart', function () {
         return {
             restrict: 'A',
             replace: true,
@@ -19,7 +62,8 @@ angular.module('octobluApp')
                 interpolate: '@',
                 showLegend: '@',
                 showXAxis: '@',
-                showYAxis: '@'
+                showYAxis: '@',
+		domain: '@'
             },
             link: function (scope, element, attr) {
                 attr.axisXFormat = attr.axisXFormat || '';
@@ -30,18 +74,24 @@ angular.module('octobluApp')
 		attr.yScale = attr.yScale || null;
 		attr.yScaleMax = attr.yScaleMax || 1;
 		attr.yScaleMin = attr.yScaleMin || 0;
-
+		attr.domain = attr.domain || null;
+		console.log("starting multibar directive");
                 /*These lines are all chart setup.  Pick and choose which chart features you want to utilize. */
-		var chart = nv.models.lineWithFocusChart();
-		  //Configure how the tooltip looks.
-	
-		  //We want to show shapes other than circles.
+		// Watch data for any changes.
+		var chart = nv.models.multiBarChart()
+	      .transitionDuration(350)
+	      .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
+	      .rotateLabels(0)      //Angle to rotate x-axis labels.
+	      //.showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
+		.stacked(false).showControls(false)
+		.groupSpacing(0.1)    //Distance between each group of bars.
+    		;
 
+		 console.log("setting x axis options");
 		  if (attr.axisXLabel) {
                         chart.xAxis.axisLabel(attr.axisXLabel);
-			chart.x2Axis.axisLabel(attr.axisXLabel);
                     }
-
+			
                   chart.xAxis.tickFormat(function (d) {
                             if (attr.axisXType && attr.axisXType === 'date') {
                                 return d3.time.format(attr.axisXFormat)(new Date(d));
@@ -51,6 +101,7 @@ angular.module('octobluApp')
                  });
 
                     // Chart y-axis settings
+		 console.log("setting y axis options");
                     if (attr.axisYLabel) {
                         chart.margin({left: 100})  // Adjust chart margins to give the y-axis some breathing room.
                         chart.yAxis.axisLabel(attr.axisYLabel);
@@ -63,15 +114,8 @@ angular.module('octobluApp')
 
                             return d3.format(attr.axisYFormat)(d);
                     });
-		   chart.x2Axis.tickFormat(function (d) {
-                            if (attr.axisXType && attr.axisXType === 'date') {
-                                return d3.time.format(attr.axisXFormat)(new Date(d));
-                            }
 
-                            return d3.format(attr.axisXFormat)(d);
-                    });
-
-
+			console.log("setting up svg");
                     var svg = d3.select(element[0])
                         .append('svg');
 
@@ -80,12 +124,17 @@ angular.module('octobluApp')
                     }
 
                     if (attr.width) {
-                        chart.style('width', attr.width + 'px');
-                    }
+                        svg.style('width', attr.width + 'px');
+                    } else {
+		    }
+		    
 
-                    // Watch data for any changes.
-                    scope.$watch('data', function (newVal) {
-                        if (newVal) {
+		scope.$watch('data', function (newVal) {
+                if (newVal) {
+	
+			    console.log("new data in multibar");
+			    console.log(scope.data);
+			    console.log(exampleData());
                             svg.datum(scope.data); // Populate the <svg> element with chart data...
                             svg.call(chart);          //Finally, render the chart!
                         }
