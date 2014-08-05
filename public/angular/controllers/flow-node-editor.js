36d4d44010d06ac944d0c0a93299f438df7aa31b
Roy van de Water <dev+roy@integrumtech.com>
2014-08-05 11:45:36 -0700

    Start storing form nodes in html

--- public/angular/controllers/flow-node-editor.js
+++ public/angular/controllers/flow-node-editor.js
@@ -2,7 +2,11 @@ angular.module('octobluApp')
   .controller('flowNodeEditorController', function ($scope, FlowNodeTypeService) {
     'use strict';
 
+    $scope.schemaControl = {};
+
     $scope.$watch('flowEditor.selectedNode', function(newFlowNode, oldFlowNode){
+      $scope.flowEditor.editorNode = _.clone($scope.flowEditor.selectedNode);
+
       if (!newFlowNode) {
         $scope.flowEditor.flowNodeType = null;
         return;
@@ -12,4 +16,8 @@ angular.module('octobluApp')
         $scope.flowEditor.flowNodeType = flowNodeType;
       });
     });
+
+    $scope.updateNode = function(){
+      _.extend($scope.flowEditor.selectedNode, $scope.flowEditor.editorNode);
+    };
 });
