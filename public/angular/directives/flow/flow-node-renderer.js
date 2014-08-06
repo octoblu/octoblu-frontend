76128657e48c8f0a3d37965df487e6d5bccf3b0b
redaphid <aaron@loqwai.com>
2014-08-06 14:36:17 -0700

    We can render up to n input ports now!

--- public/angular/directives/flow/flow-node-renderer.js
+++ public/angular/directives/flow/flow-node-renderer.js
@@ -2,13 +2,30 @@ angular.module('octobluApp')
   .service('FlowNodeRenderer', function () {
     var nodeType = {
       width: 100,
-      height: 40,
+      minHeight: 40,
       portHeight: 10,
       portWidth: 10
     };
+
+    function getNodeHeight(node) {
+      var nodeHeight = nodeType.minHeight;
+      //height of the all the ports, without spaces
+      var inputPortHeight = ((node.input) * nodeType.portHeight);
+      //40
+
+      var inputPortSpacing = ((node.input + 1) * nodeType.portHeight) / 2;
+      //25
+      inputPortHeight += inputPortSpacing;
+      if (inputPortHeight > nodeHeight) {
+        return inputPortHeight;
+      }
+      return nodeHeight;
+
+    }
+
     return {
       render: function (renderScope, node) {
-        console.log(node);
+        var nodeHeight = getNodeHeight(node);
         var nodeElement = renderScope
           .append('g')
           .classed('flow-node', true)
@@ -18,7 +35,7 @@ angular.module('octobluApp')
         nodeElement
           .append('rect')
           .attr('width', nodeType.width)
-          .attr('height', nodeType.height)
+          .attr('height', nodeHeight)
           .attr('rx', 6)
           .attr('ry', 6)
           .classed('flow-node-bg', true);
@@ -26,14 +43,14 @@ angular.module('octobluApp')
         nodeElement
           .append('text')
           .classed('flow-node-label', true)
-          .attr('y', nodeType.height / 2)
+          .attr('y', nodeHeight / 2)
           .attr('x', nodeType.width / 2)
           .attr('text-anchor', 'middle')
           .attr('alignment-baseline', 'central')
           .text(node.name || node.type);
 
         var remainingSpace =
-          nodeType.height - (node.input * nodeType.portHeight);
+          nodeHeight - (node.input * nodeType.portHeight);
 
         var spaceBetweenPorts = remainingSpace / (node.input + 1);
         var startPos = spaceBetweenPorts;
@@ -57,7 +74,7 @@ angular.module('octobluApp')
           nodeElement
             .append('rect')
             .attr('x', nodeType.width - (nodeType.portWidth / 2))
-            .attr('y', (nodeType.height / 2) - 5)
+            .attr('y', (nodeHeight / 2) - 5)
             .attr('width', nodeType.portWidth)
             .attr('height', nodeType.portHeight)
             .classed('flow-node-port', true)
--- test/angular/services/flow-node-renderer-spec.js
+++ test/angular/services/flow-node-renderer-spec.js
@@ -72,10 +72,24 @@ describe('FlowNodeRenderer', function () {
     it('should place 3 input ports evenly spaced on the left', function(){
       sut.render(renderScope, {id: '1', input: 3});
       var ports = renderScope.selectAll('.flow-node-input-port')[0];
+      var node = $(renderScope.selectAll('.flow-node > rect')[0]);
+      expect(node.attr('height')).to.equal('50');
+      expect(Math.round($(ports[0]).attr('y'))).to.equal(5);
+      expect(Math.round($(ports[1]).attr('y'))).to.equal(20);
+      expect(Math.round($(ports[2]).attr('y'))).to.equal(35);
+    });
 
-      expect(Math.round($(ports[0]).attr('y'))).to.equal(3);
-      expect(Math.round($(ports[1]).attr('y'))).to.equal(15);
-      expect(Math.round($(ports[2]).attr('y'))).to.equal(28);
+    it('should place 4 input ports evenly spaced on the left', function(){
+      sut.render(renderScope, {id: '1', input: 4});
+      var ports = renderScope.selectAll('.flow-node-input-port')[0];
+
+      var node = $(renderScope.selectAll('.flow-node > rect')[0]);
+      expect(node.attr('height')).to.equal('65');
+
+//      expect(Math.round($(ports[0]).attr('y'))).to.equal(3);
+//      expect(Math.round($(ports[1]).attr('y'))).to.equal(15);
+//      expect(Math.round($(ports[2]).attr('y'))).to.equal(28);
+//      expect(Math.round($(ports[3]).attr('y'))).to.equal(28);
     });
 
   });
