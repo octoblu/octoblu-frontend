5c9c0f7ccc938e1935e62a1346b69ca43dc1e280
redaphid <aaron@loqwai.com>
2014-08-11 14:29:03 -0700

    getLinks, getNodes, filterable getNodes.

--- test/angular/models/flow-model-spec.js
+++ test/angular/models/flow-model-spec.js
@@ -18,20 +18,27 @@ describe('FlowModel', function () {
 
   it('should add a node to the list of flowNodes', function () {
     sut.addNode({});
-    expect(sut.nodes.length).to.equal(1);
+    expect(sut.getNodes().length).to.equal(1);
   });
 
-  describe('add links', function(){
-    
+  describe('add links', function () {
 
-    it('should exist', function(){
+
+    it('should exist', function () {
       expect(sut.addLink).to.exist;
     });
-    
-    it('should add a link to the list of flow links', function(){
+
+    it('should add a link to the list of flow links', function () {
       sut.addLink({});
-      expect(sut.links.length).to.equal(1);
-     });
+      expect(sut.getLinks().length).to.equal(1);
+    });
+
+    it('should return links for a specified node', function () {
+      sut.addNode({ id: '1'});
+      sut.addLink({ from: '1', to: '1' });
+      sut.addLink({ from: '2', to: '2' });
+      expect(sut.getLinks({forNode: '1'}).length).to.equal(1);
+    });
 
   });
 
