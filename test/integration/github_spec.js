bca51a6ebf2e083dadb979d2a4365f0b082aded8
Roy van de Water <dev+roy@integrumtech.com>
2014-08-12 10:48:23 -0700

    Add better images migration

--- test/integration/github_spec.js
+++ test/integration/github_spec.js
@@ -5,57 +5,6 @@ var webdriver = require('selenium-webdriver');
 var SeleniumServer = require('selenium-webdriver/remote').SeleniumServer;
 var fs = require('fs');
 
-xdescribe('Octoblu Login', function () {
-  this.timeout(10000);
-
-  var driver, server;
-  beforeEach(function () {
-    server = new SeleniumServer('/usr/local/bin/selenium-server-standalone-2.42.2.jar', {
-      port: 4444
-    });
-    server.start();
-    driver = new webdriver
-                .Builder()
-                .usingServer(server.address())
-                .withCapabilities(webdriver.Capabilities.firefox())
-                .build();
-  });
-
-  afterEach(function () {
-    driver.quit();
-    server.stop();
-  });
-
-  describe('when I go to the login page', function () {
-    beforeEach(function (done) {
-      driver.get('http://localhost:8080/login').then(done);
-    });
-
-    describe('and I fill in the login form', function () {
-      beforeEach(function (done) {
-        var email    = driver.findElement(webdriver.By.name('email'));
-        email.sendKeys('john.connor@mailinator.com');
-        var password = driver.findElement(webdriver.By.name('password'));
-        password.sendKeys('judgementday');
-        password.submit().then(done);
-      });
-
-      it('should login', function (done) {
-        driver.takeScreenshot().then(function(data) {
-          fs.writeFileSync('/tmp/render.png', data, 'base64');
-          done();
-        });
-
-        // var h1 = driver.findElement(webdriver.By.tagName('h1'));
-        // h1.getText().then(function(text){
-        //   expect(text).to.contain('Octoblu');
-        //   done();
-        // });
-      });
-    });
-  });
-});
-
 describe('Octoblu Login', function () {
   this.timeout(30000);
   var server, client;
@@ -74,37 +23,57 @@ describe('Octoblu Login', function () {
 
     describe('going to skynet', function () {
       beforeEach(function (done) {
-        client = webdriverjs.remote({ host: 'localhost', port: 4444 }).init().call(done);
+        client = webdriverjs.remote({
+          host: 'localhost',
+          port: 4444,
+          logLevel: 'verbose',
+        }).init().call(done);
       });
 
       afterEach(function (done) {
         client.endAll(done);
       });
 
-      describe('when the login form is filled out', function () {
+      describe('when navigating to the login page', function () {
         beforeEach(function (done) {
           client
-          .url('http://app.octoblu.com/login')
-          .waitFor('input[name=email]')
-          .setValue('input[name=email]', 'sqrt@octoblu.com')
-          .setValue('input[name=password]', 'asdf')
-          .submitForm('form')
           .call(done);
         });
 
-        describe('activating github', function () {
+        describe('Getting to the add github page', function () {
           beforeEach(function (done) {
             client
-            .waitFor('a[text="Connect"]')
-            .click('a[text="Connect"]')
-            .waitFor('a div[title=Github]')
+            .url('http://app.octoblu.com/login')
+            .waitFor('input[name=email]')
+            .setValue('input[name=email]', 'sqrt@octoblu.com')
+            .setValue('input[name=password]', 'asdf')
+            .submitForm('form')
+            .waitFor('i.fa-code-fork', 2000) // Home page has loaded
+            .url('http://app.octoblu.com/node-wizard')
+            .waitFor('a div[title=Github]', 20000)
             .click('a div[title=Github]')
-            .buttonClick('.btn-primary')
             .call(done);
           });
 
-          it('should take a screenshot', function (done) {
-            client.saveScreenshot('/tmp/render.png').call(done);
+          describe('when navigating to the wizard', function () {
+            describe('when I activate', function () {
+              beforeEach(function (done) {
+                client
+                .waitFor('.btn-primary', 20000)
+                .pause(5000)
+                .click('.btn-primary')
+                .waitFor('input[name="login"]', 20000)
+                .setValue('input[name="login"]', 'square-root-of-saturn')
+                .setValue('input[name="password"]', 'INSERT_SECRET_HERE')
+                .click('input[name="commit"]')
+                .waitFor('*=Authorize')
+                .call(done);
+              });
+
+              it('should take a screenshot', function (done) {
+                client.saveScreenshot('/tmp/render.png').call(done);
+              });
+            });
           });
         });
       });
