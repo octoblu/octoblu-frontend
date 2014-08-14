0d789887962e1831d177127fdf089692a832bd4b
Jade Meskill <jade.meskill@gmail.com>
2014-08-14 10:48:50 -0700

    use replicaset

--- config/database.js
+++ config/database.js
@@ -16,9 +16,8 @@ var config = {
         redisSessionUrl: 'redis://meshblu-redis.csy8op.0001.usw2.cache.amazonaws.com'
     },
     production: {
-        // url : 'mongodb://[user]:[password]@dharma.mongohq.com:10040/meshines'
-        url : 'mongodb://172.31.44.170:27017/meshines',
-        skynetUrl: 'mongodb://172.31.44.146:27017/skynet',
+        url : 'mongodb://172.31.33.28:27017/octoblu,mongodb://172.31.38.108:27017/octoblu,mongodb://172.31.32.97:27017/octoblu',
+        skynetUrl : 'mongodb://172.31.33.28:27017/meshblu,mongodb://172.31.38.108:27017/meshblu,mongodb://172.31.32.97:27017/meshblu',
         redisSessionUrl: 'redis://meshblu-redis.csy8op.0001.usw2.cache.amazonaws.com'
     }
 };
