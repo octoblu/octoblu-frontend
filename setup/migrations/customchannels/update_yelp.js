c6b26f25497d494318a7b32fdfe899e69c4339e8
josh coffman <josh@computeristsolutions.com>
2014-08-15 13:03:53 -0700

    updated yelp oauth info

--- setup/migrations/customchannels/update_yelp.js
+++ setup/migrations/customchannels/update_yelp.js
@@ -8,10 +8,14 @@ var apiInfo = {
 	documentation	: '',
 	useCustom		: true,
 	logo			: 'http://www.greathillsent.com/images/yelp.png',
-	oauth 			: {
-		version			: "1.0",
-		key 			: "",
-		secret 			: ""
+	oauth: {
+		key: "_JhMV3MO8DAtX9kotuD5jw",
+		secret: "rMmUsRdAYAcOtxU_-tZRXO7Fbyc",
+		version: "1.0",
+		clientId: "w5Ds7zP3v1JFIKo9r4pVVQ",
+		is0LegAuth: true,
+		accessToken: "IIK0VuJ6Pi7fejzBnvmSvFbPBn1qfLJE",
+		accessSecret: "7fTa6NY0wDX9VCoJlx2-IOX6kB4"
 	}
 };
 
