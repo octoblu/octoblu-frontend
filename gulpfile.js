9217b5809d37476a76b6b1cbfb0277aa4b912612
Jade Meskill <jade.meskill@gmail.com>
2014-08-19 14:29:35 -0700

    auto-bower and concat using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
-var   gulp = require('gulp'),
-    concat = require('gulp-concat'),
-      less = require('gulp-less'),
-   plumber = require('gulp-plumber'),
-sourcemaps = require('gulp-sourcemaps');
+var gulp         = require('gulp'),
+  bower          = require('gulp-bower'),
+  mainBowerFiles = require('main-bower-files'),
+  concat         = require('gulp-concat'),
+  less           = require('gulp-less'),
+  plumber        = require('gulp-plumber'),
+  sourcemaps     = require('gulp-sourcemaps');
+
+gulp.task('bower', function() {
+  bower('./public/lib');
+  return gulp.src(mainBowerFiles({filter: /\.js/}))
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+      .pipe(concat('dependencies.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/assets/javascripts/dist/'));
+});
 
 gulp.task('less:compile', function(){
   gulp.src('./assets/less/manifest.less')
@@ -21,10 +33,11 @@ gulp.task('javascript:concat', function(){
     .pipe(gulp.dest('./public/assets/javascripts/dist/'));
 });
 
-gulp.task('default', ['less:compile', 'javascript:concat'], function() {
+gulp.task('default', ['bower', 'less:compile', 'javascript:concat'], function() {
 });
 
 gulp.task('watch', ['default'], function() {
+  gulp.watch(['./bower.json'], ['bower']);
   gulp.watch(['./assets/less/**/*.less'], ['less:compile']);
   gulp.watch(['./public/angular/**/*.js'], ['javascript:concat']);
 });
