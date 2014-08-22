d3d768d83ac948bbed2bf38e9e91d9c90d672ac6
kmariano <koshin.mariano@gmail.com>
2014-08-21 17:41:21 -0700

    Filtering node types in omnibox v1

--- public/angular/directives/flow/omnibox/omnibox.js
+++ public/angular/directives/flow/omnibox/omnibox.js
@@ -4,6 +4,20 @@ angular.module('octobluApp')
       restrict: 'E',
       replace: true,
       templateUrl: '/pages/flow-editor-omnibox.html',
+      scope : {
+        nodes : '='
+      },
+      link: function (scope, element) {
+
+
+        scope.filterResults = function(searchText){
+          var filteredNodes = _.filter(nodes, function(node){
+
+          });
+          return filteredNodes;
+        }
+
+      }
     };
 
   });
