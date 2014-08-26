88276426b7aa0e097d62d47cd3b758110d013f49
kmariano <koshin.mariano@gmail.com>
2014-08-26 15:34:39 -0700

    Adding tabs to the flow-browser

--- public/angular/directives/flow/flow-browser/flow-browser.js
+++ public/angular/directives/flow/flow-browser/flow-browser.js
@@ -11,6 +11,20 @@ angular.module('octobluApp')
       },
 
       controller: function ($scope, FlowNodeTypeService) {
+        var tabs = {
+          nodes: {
+            name: 'nodes',
+            template: '/angular/directives/flow/flow-browser/flow-browser-nodes.html'
+          }
+        };
+
+        $scope.activeTab = tabs.node;
+
+        $scope.setActiveTab = function(tabName) {
+          $scope.maximized = true;
+          $scope.activeTab = tabs[tabName];
+        };
+
         $scope.addFlowNodeType = function(flowNodeType) {
             var newFlowNode = FlowNodeTypeService.createFlowNode(flowNodeType);
             $scope.flow.addNode(newFlowNode);
