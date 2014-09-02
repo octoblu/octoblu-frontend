15c1a4bb743e42681845c293ab2c13d3044ded01
icereval <icereval@gmail.com>
2014-09-02 14:30:53 -0700

    allow users to claim existing devices not on their network.

--- public/angular/controllers/add-device.js
+++ public/angular/controllers/add-device.js
@@ -1,42 +1,57 @@
 'use strict';
 
 angular.module('octobluApp')
-    .controller('addDeviceController', function($scope, $state, $stateParams, NodeTypeService, deviceService) {
-        $scope.newDevice = {};
+  .controller('addDeviceController', function ($scope, $state, $stateParams, NodeTypeService, deviceService) {
+    $scope.newDevice = {};
+    $scope.existingDevice = {};
 
-        NodeTypeService.getNodeTypes().then(function(nodeTypes){
-            $scope.nodeType = _.findWhere(nodeTypes, {_id: $stateParams.nodeTypeId});
-        })
-        .then(function(){
-            return deviceService.getUnclaimed($scope.nodeType.skynet.type);
-        })
-        .then(function(unclaimedDevices){
-            $scope.newDevice.unclaimedDevices = unclaimedDevices;
-            $scope.newDevice.selectedDevice   = _.first(unclaimedDevices);
-        });
+    NodeTypeService.getNodeTypes().then(function (nodeTypes) {
+      $scope.nodeType = _.findWhere(nodeTypes, {_id: $stateParams.nodeTypeId});
+    })
+      .then(function () {
+        return deviceService.getUnclaimed($scope.nodeType.skynet.type);
+      })
+      .then(function (unclaimedDevices) {
+        $scope.newDevice.unclaimedDevices = unclaimedDevices;
+        $scope.newDevice.selectedDevice = _.first(unclaimedDevices);
+        $scope.newDevice.unclaimedDevices.unshift({type: 'existing'});
+      });
 
-        $scope.addDevice = function() {
-          var deviceOptions, promise;
-          delete $scope.errorMessage;
+    $scope.getDeviceLabel = function (device) {
+      if (device.type === 'existing') {
+        return 'Claim Existing';
+      }
+      return device.uuid + '(' + (device.type || 'device') + ')';
+    };
 
-          deviceOptions = {
-            type: $scope.nodeType.skynet.type,
-            subtype: $scope.nodeType.skynet.subtype,
-            name: $scope.newDevice.name
-          };
+    $scope.addDevice = function () {
+      var deviceOptions, promise;
+      delete $scope.errorMessage;
 
-          if($scope.newDevice.selectedDevice) {
-            deviceOptions.uuid = $scope.newDevice.selectedDevice.uuid;
-            promise = deviceService.claimDevice(deviceOptions);
-          } else {
-            promise = deviceService.registerDevice(deviceOptions);
-          }
+      deviceOptions = {
+        type: $scope.nodeType.skynet.type,
+        subtype: $scope.nodeType.skynet.subtype,
+        name: $scope.newDevice.name
+      };
 
-          promise.then(function(){
-            $state.go("ob.connector.nodes.all");
-          }, function(error){
-            $scope.errorMessage = error;
-          });
+      if ($scope.newDevice.selectedDevice) {
+        if ($scope.newDevice.selectedDevice.type === 'existing') {
+          deviceOptions.uuid = $scope.existingDevice.uuid;
+          deviceOptions.token = $scope.existingDevice.token;
+        } else {
+          deviceOptions.uuid = $scope.newDevice.selectedDevice.uuid;
+        }
 
-        };
-    });
+        promise = deviceService.claimDevice(deviceOptions);
+      } else {
+        promise = deviceService.registerDevice(deviceOptions);
+      }
+
+      promise.then(function () {
+        $state.go("ob.connector.nodes.all");
+      }, function (error) {
+        $scope.errorMessage = error;
+      });
+
+    };
+  });
