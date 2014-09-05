3cc48267f55f17e3485afd782361cd963d0d0acc
Jade Meskill <jade.meskill@gmail.com>
2014-09-04 20:00:31 -0700

    fix noauth

--- public/angular/controllers/add-channel-noauth.js
+++ public/angular/controllers/add-channel-noauth.js
@@ -3,10 +3,10 @@
 angular.module('octobluApp')
     .controller('addChannelNoauthController', function($scope, $state, nodeType, currentUser, userService, channelService) {
         $scope.activate = function(){
-            userService.activateNoAuthChannel(currentUser.skynetuuid, nodeType.channel._id, function (data) {
+            userService.activateNoAuthChannel(currentUser.skynetuuid, nodeType.channelid, function (data) {
                 channelService.getActiveChannels(true);
                 channelService.getAvailableChannels(true);
-                $state.go('ob.connector.nodes.channel-detail', {id: nodeType.channel._id});
+                $state.go('ob.connector.nodes.channel-detail', {id: nodeType.channelid});
             });
         };
     });
