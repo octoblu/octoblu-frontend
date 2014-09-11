deaf5caca0b84ad493f04b2cd7319b923762942a
Roy van de Water <dev+roy@integrumtech.com>
2014-09-11 10:15:54 -0700

    Restore flow channel form controller

--- public/angular/controllers/flow-channel-form-controller.js
+++ public/angular/controllers/flow-channel-form-controller.js
@@ -1,7 +1,47 @@
 angular.module('octobluApp')
 .controller('FlowChannelFormController', function($scope, channelService) {
   'use strict';
-  channelService.getById($scope.node.channelid).then(function(channel){
-    $scope.resources = channel.application.resources;
+
+  var getResources = function(){
+    if(!$scope.node.channelid){
+      return $scope.resources = null;
+    }
+
+    channelService.getById($scope.node.channelid).then(function(channel){
+      $scope.resources = channel.application.resources;
+      $scope.channel = channel;
+    });
+  };
+
+  $scope.getEndpointLabel = function(resource) {
+    return resource.httpMethod + ' ' + resource.path;
+  };
+
+  var selectEndpoint = function(){
+    var node, resources, selectedEndpoint;
+
+    node = $scope.node;
+    resources = $scope.resources;
+    selectedEndpoint = _.findWhere(resources, {path: node.path, httpMethod: node.method});
+    $scope.selectedEndpoint = selectedEndpoint || _.first(resources);
+  };
+
+  $scope.$watch('selectedEndpoint', function(){
+    console.log($scope.selectedEndpoint);
+    if(!$scope.selectedEndpoint){
+      return;
+    }
+
+    if($scope.selectedEndpoint.fullURL) {
+      $scope.node.url = $scope.selectedEndpoint.fullURL;
+    } else {
+      $scope.node.url = $scope.channel.application.base + $scope.selectedEndpoint.path;
+    }
+    $scope.node.method = $scope.selectedEndpoint.httpMethod;
   });
+
+  $scope.$watch('node',        getResources);
+  $scope.$watch('node.path',   selectEndpoint);
+  $scope.$watch('node.method', selectEndpoint);
+  $scope.$watch('resources',   selectEndpoint);
 });
