29bf6813abaaddbf030e02860701659b5f2b48f5
Roy van de Water <dev+roy@integrumtech.com>
2014-09-12 09:20:24 -0700

    Break out channels into their own files

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp         = require('gulp'),
   bower          = require('gulp-bower'),
   mainBowerFiles = require('main-bower-files'),
   concat         = require('gulp-concat'),
+  jsoncombine    = require('gulp-jsoncombine'),
   less           = require('gulp-less'),
   plumber        = require('gulp-plumber'),
-  sourcemaps     = require('gulp-sourcemaps');
+  sourcemaps     = require('gulp-sourcemaps'),
+  _              = require('lodash');
 
 gulp.task('bower', function() {
   bower('./public/lib');
@@ -38,11 +40,20 @@ gulp.task('javascript:concat', function(){
     .pipe(gulp.dest('./public/assets/javascripts/dist/'));
 });
 
-gulp.task('default', ['bower:concat', 'less:compile', 'javascript:concat'], function() {
+gulp.task('channels:concat', function(){
+  gulp.src('./assets/json/channels/*.json')
+    .pipe(jsoncombine('channels.json', function(data){
+      return new Buffer(JSON.stringify(_.values(data)));
+    }))
+    .pipe(gulp.dest('./assets/json/'));
+});
+
+gulp.task('default', ['bower:concat', 'less:compile', 'javascript:concat', 'channels:concat'], function() {
 });
 
 gulp.task('watch', ['default'], function() {
   gulp.watch(['./bower.json'], ['bower']);
   gulp.watch(['./assets/less/**/*.less'], ['less:compile']);
   gulp.watch(['./public/angular/**/*.js'], ['javascript:concat']);
+  gulp.watch(['./assets/json/channels/*.json'], ['channels:compile']);
 });
