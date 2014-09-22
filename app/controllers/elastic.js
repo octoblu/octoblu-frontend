6f572d56f602f920aa86ed09d925755896ca54e5
Jade Meskill <jade.meskill@gmail.com>
2014-09-22 16:55:04 -0700

    use new elastic search

--- app/controllers/elastic.js
+++ app/controllers/elastic.js
@@ -16,7 +16,7 @@ module.exports = function (app) {
 
   app.all('/api/elastic/skynet_trans_log/_search', function(req, res){
     request({
-      url:    'http://dmz-elb-elasticsearch-1064045065.us-west-2.elb.amazonaws.com/skynet_trans_log/_search',
+      url:    'http://54.68.191.122/skynet_trans_log/_search',
       method: req.method,
       json:   req.body
     }, function(error, incomingMessage, response){
