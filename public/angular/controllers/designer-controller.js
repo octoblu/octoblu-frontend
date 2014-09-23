dd8f587c905fb477f2b76cab8192cf45a39c4148
redaphid <iam@aaronherres.com>
2014-09-23 15:49:51 -0700

    moved the functions related to switching between flows to the designer controller.

--- public/angular/controllers/designer-controller.js
+++ public/angular/controllers/designer-controller.js
@@ -1,7 +1,41 @@
 angular.module('octobluApp')
   .controller('DesignerController', function ($state, FlowService) {
+    
     FlowService.getAllFlows()
       .then(function (flows) {
+      	$state.flows = flows;
+      	$state.activeFlow = _.findWhere($scope.flows, {flowId: $stateParams.flowId});
+
         $state.go('flow', {flowId: _.first(flows).flowId});
     });
+
+    $scope.getActiveFlow = function(){
+      return FlowService.getActiveFlow();
+    };
+
+    $scope.setActiveFlow = function (flow) {
+      $scope.activeFlow = flow;
+      FlowService.setActiveFlow($scope.activeFlow);
+    };
+
+    $scope.isActiveFlow = function (flow) {
+      return flow === $scope.activeFlow;
+    };
+
+    $scope.addFlow = function () {
+      var name = 'Flow ' + ($scope.flows.length + 1);
+      var newFlow = FlowService.newFlow({name: name});
+      FlowService.saveFlow(newFlow).then(function(){
+        $state.go('flow', {flowId: newFlow.flowId});
+      });
+    };
+
+    $scope.deleteFlow = function (flow) {
+      var deleteFlowConfirmed = $window.confirm('Are you sure you want to delete ' + flow.name + '?');
+      if (deleteFlowConfirmed) {
+        FlowService.deleteFlow(flow.flowId).then(function () {
+          $state.go('design')
+        });
+      }
+    };
 });
