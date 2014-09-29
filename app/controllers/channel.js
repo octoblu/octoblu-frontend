a3cffba00fdd1a4fd4a465b46172abe6784b6f9b
Roy van de Water <dev+roy@integrumtech.com>
2014-09-29 11:03:24 -0700

    Cover up our private credentials

--- app/controllers/channel.js
+++ app/controllers/channel.js
@@ -1,35 +1,44 @@
 'use strict';
 
-var _ = require('lodash'),
-    mongoose = require('mongoose'),
-    User = mongoose.model('User'),
-    Channel = require('../models/channel'),
-    isAuthenticated = require('./middleware/security').isAuthenticated;
-var ObjectId = require('mongoose').Types.ObjectId;
+var _               = require('lodash');
+var mongoose        = require('mongoose');
+var Channel         = require('../models/channel');
+var isAuthenticated = require('./middleware/security').isAuthenticated;
+
+var User     = mongoose.model('User');
+var ObjectId = mongoose.Types.ObjectId;
 
 module.exports = function (app) {
-    app.get('/api/channels', function (req, res) {
-        Channel.findAllPublic().then(function(channels){
-            res.send(channels)
-        });
-    });
+  var pickProperties, whiteListProperties;
 
-    app.get('/api/customchannels', isAuthenticated, function (req, res) {
-        Channel.findAllByOwnerId(req.user.resource.uuid).then(function(channels){
-            res.send(channels)
-        });
+  app.get('/api/channels', function (req, res) {
+    Channel.findAllPublic().then(function(channels){
+      res.send(whiteListProperties(channels));
     });
+  });
 
-    app.get('/api/channels/active', isAuthenticated, function (req, res) {
-        var channelIds = _.pluck(req.user.api, 'channelid');
-        Channel.findByIds(channelIds).then(function(channels){
-            res.send(channels);
-        });
+  app.get('/api/channels/active', isAuthenticated, function (req, res) {
+    var channelIds = _.pluck(req.user.api, 'channelid');
+    Channel.findByIds(channelIds).then(function(channels){
+      res.send(whiteListProperties(channels));
     });
+  });
 
-    app.get('/api/channels/:id', function (req, res) {
-        Channel.findById(req.params.id).then(function(channel){
-            res.send(channel)
-        });
+  app.get('/api/channels/:id', function (req, res) {
+    Channel.findById(req.params.id).then(function(channel){
+      res.send(whiteListProperties(channel));
     });
+  });
+
+  pickProperties = function(channel){
+    return _.pick(channel, '_id', 'name', 'application', 'type', 'auth_strategy');
+  }
+
+  whiteListProperties = function(channels) {
+    if(!_.isArray(channels)) {
+      return pickProperties(channels);
+    }
+
+    return _.map(channels, pickProperties);
+  };
 };
