37249c2d54c08a0730e9baf1b05d34ecf0c9d556
Peter DeMartini <peterdemartini@me.com>
2014-10-06 10:52:03 -0700

    Moved oauth to the salesforce channel's json

--- config/salesforce.js
+++ config/salesforce.js
@@ -1,27 +1,12 @@
 var SalesForceStrategy = require('passport-forcedotcom').Strategy;
 var mongoose = require('mongoose');
 var User     = mongoose.model('User');
+var Channel = require('../app/models/channel');
 
-var CONFIG = {
-  development: {
-    clientID:     'INSERT_SECERT_HERE',
-    clientSecret: 'INSERT_SECERT_HERE',
-    callbackURL:    'http://localhost:8080/api/oauth/salesforce/callback'
-  },
-  production: {
-    clientID:     'INSERT_SECERT_HERE',
-    clientSecret: 'INSERT_SECERT_HERE',
-    callbackURL:    'https://app.octoblu.com/api/oauth/salesforce/callback'
-  },
-  staging: {
-    clientID:     'INSERT_SECERT_HERE',
-    clientSecret: 'INSERT_SECERT_HERE',
-    callbackURL:    'https://staging.octoblu.com/api/oauth/salesforce/callback'
-  }
-}[process.env.NODE_ENV];
+var channel = Channel.syncFindByType('channel:salesforce');
+var CONFIG = channel.oauth[process.env.NODE_ENV];
 
 CONFIG.passReqToCallback = true;
-CONFIG.tokenMethod = 'access_token_bearer';
 CONFIG.scope = ['id','chatter_api', 'api', 'full', 'refresh_token', 'visualforce', 'web'];
 
 var salesForceStrategy = new SalesForceStrategy(CONFIG, function(req, accessToken, refreshToken, profile, done){
