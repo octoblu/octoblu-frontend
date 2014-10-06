5ee74256ded678bfa0e91b36c7fa7263bfc3b953
kmariano <koshin.mariano@gmail.com>
2014-10-06 14:57:22 -0700

    Removing escaping of characters for flow properties.

--- app/models/flow.js
+++ app/models/flow.js
@@ -79,11 +79,9 @@ FlowSchema.statics.deleteByFlowIdAndUserUUID = function (flowId, userUUID, meshb
 };
 
 FlowSchema.statics.updateByFlowIdAndUser = function (flowId, userUUID, flowData) {
-  var escapedFlowData = escapeFlowData(flowData);
-
   var query = {flowId: flowId, 'resource.owner.uuid': userUUID};
 
-  return this.update(query, escapedFlowData).exec().then(function (numAffected) {
+  return this.update(query, flowData).exec().then(function (numAffected) {
     if (numAffected !== 1) {
       throw new Error('404')
     }
@@ -92,65 +90,13 @@ FlowSchema.statics.updateByFlowIdAndUser = function (flowId, userUUID, flowData)
 
 FlowSchema.statics.getFlows = function (userUUID) {
   var self = this;
-  return self.find({'resource.owner.uuid': userUUID}).lean().exec().then(
-    function(flows){
-      return _.map(flows, unescapeFlowData);
-    });
+  return self.find({'resource.owner.uuid': userUUID}).lean().exec();
 };
 
 FlowSchema.statics.getFlow = function (flowId) {
   var self = this;
-  return self.findOne({'flowId': flowId}).lean().exec().then(
-    function(flow){
-      return unescapeFlowData(flow);
-    });
+  return self.findOne({'flowId': flowId}).lean().exec();
 };
 
-
-function escapeFlowData(flowData) {
-  var escapedFlowData = _.clone(flowData);
-  escapedFlowData.nodes = _.map(flowData.nodes, function (node) {
-    return escapeNodeProperties(node);
-  });
-
-  return escapedFlowData;
-}
-
-function unescapeFlowData(flowData) {
-  var unescapedFlowData = _.clone(flowData);
-  unescapedFlowData.nodes = _.map(flowData.nodes, function (node) {
-    return unescapeNodeProperties(node);
-  });
-
-  return unescapedFlowData;
-}
-
-function escapeNodeProperties(node) {
-  if(!_.isObject(node)) {
-    return node;
-  }
-
-  var newNode = {};
-  _.each(_.keys(node), function (key) {
-    var newKey = key.replace(/\./g, '%2E');
-    newNode[newKey] = escapeNodeProperties(node[key]);
-  });
-  return newNode;
-}
-
-function unescapeNodeProperties(node) {
-  if(!_.isObject(node)) {
-    return node;
-  }
-
-  var newNode = {};
-
-  _.each(_.keys(node), function (key) {
-    var newKey = key.replace(/%2E/g, '.');
-    newNode[newKey] = unescapeNodeProperties(node[key]);
-  });
-
-  return newNode;
-}
 
 module.exports = FlowSchema;
