95041625705acf4b424b52707e90d94b15f3638e
Peter DeMartini <peterdemartini@me.com>
2014-10-21 17:15:42 -0700

    Crisply is now working

--- app/models/flow-deploy.js
+++ app/models/flow-deploy.js
@@ -63,7 +63,7 @@ var FlowDeploy = function(options){
       node.oauth = {};
       var userApiMatch = _.findWhere(userApis, {'_id': new mongoose.Types.ObjectId(node.channelActivationId)});
       if (userApiMatch) {
-        node.oauth.access_token = userApiMatch.token;
+        node.oauth.access_token = userApiMatch.token || userApiMatch.key;
         node.oauth.access_token_secret = userApiMatch.secret;
       }
       var channelApiMatch = _.findWhere(channelApis, {'_id': node.channelid});
