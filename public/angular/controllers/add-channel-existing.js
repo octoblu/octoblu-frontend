97695a10f9c53393d5bda94f88e75b01b780268a
Peter DeMartini <peterdemartini@me.com>
2014-10-28 14:04:34 -0700

    Fixed existing channel and agilezen

--- public/angular/controllers/add-channel-existing.js
+++ public/angular/controllers/add-channel-existing.js
@@ -2,5 +2,8 @@
 
 angular.module('octobluApp')
 	.controller('addChannelExistingController', function($scope, $state, nodeType, channelService) {
-
+		channelService.getChannelActivationById(nodeType.channelid)
+			.then(function(channelActivation){
+		    $scope.existingChannel = channelActivation;
+		  });
 	});
