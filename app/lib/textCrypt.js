1a04fdb2966bf7b06c26605a324e96ed620910f3
Jade Meskill <jade.meskill@gmail.com>
2014-11-03 10:50:45 -0700

    fix encryption

--- app/lib/textCrypt.js
+++ app/lib/textCrypt.js
@@ -5,15 +5,17 @@ var iv = new Buffer(process.env.TEXT_CRYPT_NONCE || 'thisisanoncense1');
 var AESCrypt = {};
 
 AESCrypt.decrypt = function(encryptdata) {
-  var decipher = crypto.createDecipheriv('aes-256-cbc', cryptkey, iv);
-  decipher.update(encryptdata, 'hex');
-  return decipher.final('utf8');
+  var decypher = crypto.createDecipheriv('aes-256-cbc', cryptkey, iv);
+  var decyphered = decypher.update(encryptdata, 'hex', 'utf8');
+  decyphered += decypher.final('utf8');
+  return decyphered;
 }
 
 AESCrypt.encrypt = function(cleardata) {
-  var encipher = crypto.createCipheriv('aes-256-cbc', cryptkey, iv);
-  encipher.update(cleardata, 'utf8');
-  return encipher.final('hex');
+  var encypher = crypto.createCipheriv('aes-256-cbc', cryptkey, iv);
+  var encyphered = encypher.update(cleardata, 'utf8');
+  encyphered += encypher.final('hex');
+  return encyphered
 }
 
 module.exports = AESCrypt;
