1c1d4fac82c73d5a7fb25db34c5a40b40a184dae
alisamfp <eatliightning@gmail.com>
2014-11-05 16:38:57 -0700

    added cloud api

---
+++ app/controllers/channel-clouddotcom-controller.js
@@ -0,0 +1,21 @@
+var User = require('../models/user');
+var textCrypt = require('../lib/textCrypt');
+function ChannelCloudDotComController(){
+  var self = this;
+
+  self.create = function(req, res){
+    var channelId = req.params.id;
+
+    req.user.overwriteOrAddApiByChannelId(channelId, { authtype: 'clouddotcom', token_crypt : textCrypt.encrypt(req.body.username), secret_crypt : textCrypt.encrypt(req.body.password) });
+    req.user.save(function(err){
+      if(err){
+        console.log('Error saving user', err);
+        res.send(422);
+        return;
+      }
+      res.send(201);
+    });
+  };
+}
+
+module.exports = ChannelCloudDotComController;
