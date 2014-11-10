b0be5bf05dc5ed8060ea788e3575f254bce24903
Jade Meskill <jade.meskill@gmail.com>
2014-11-10 14:21:27 -0700

    start supporting nedb

--- app/lib/database.js
+++ app/lib/database.js
@@ -1,58 +1,58 @@
 'use strict';
 var config  = require('../../config/database')(),
   when      = require('when'),
-	nodefn    = require('when/node/function'),
-	_ 				= require('lodash'),
-	path 			= require('path');
+  nodefn    = require('when/node/function'),
+  _         = require('lodash'),
+  path      = require('path');
 
 function createNeDBCollection(collection){
-	var Datastore = require('nedb');
+  var Datastore = require('nedb');
   return new Datastore({
-    filename: path.join(__dirname, '/../database/' + collection + '.db'),
+    filename: path.join(config.databaseDirectory, collection + '.db'),
     autoload: true
   });
 }
 
 function createObjectId(id) {
-	var mongojs = require('mongojs');
-	return mongojs.ObjectId(id);
+  var mongojs = require('mongojs');
+  return mongojs.ObjectId(id);
 }
 
 function wrapCollection(collection){
-	return {
-	  find: nodefn.lift(_.bind(collection.find, collection)),
-	  findOne: nodefn.lift(_.bind(collection.findOne, collection)),
-	  remove: nodefn.lift(_.bind(collection.remove, collection)),
-	  insert: nodefn.lift(_.bind(collection.insert, collection)),
-	  save: nodefn.lift(_.bind(collection.save, collection)),
-	  update: nodefn.lift(_.bind(collection.update, collection)),
-	  ObjectId: createObjectId
-	};
+  return {
+    find: nodefn.lift(_.bind(collection.find, collection)),
+    findOne: nodefn.lift(_.bind(collection.findOne, collection)),
+    remove: nodefn.lift(_.bind(collection.remove, collection)),
+    insert: nodefn.lift(_.bind(collection.insert, collection)),
+    save: nodefn.lift(_.bind(collection.save, collection)),
+    update: nodefn.lift(_.bind(collection.update, collection)),
+    ObjectId: createObjectId
+  };
 }
 
 // Returns a hash of collections
 function Database(){
-	return this;
+  return this;
 }
 
 Database.prototype.createConnection = function(options){
-	var self = this;
-	options = options || {};
-	self.config = config;
+  var self = this;
+  options = options || {};
+  self.config = config;
 
-	if(config.databaseType === 'nedb'){
-		self.getCollectionBase = createNeDBCollection;
-	}else{
-		var mongojs = require('mongojs');
-	  self.db = mongojs(config.mongojsUrl);
-	  self.getCollectionBase = _.bind(self.db.collection, self.db);
-	}
+  if(config.databaseType === 'nedb'){
+    self.getCollectionBase = createNeDBCollection;
+  }else{
+    var mongojs = require('mongojs');
+    self.db = mongojs(config.mongojsUrl);
+    self.getCollectionBase = _.bind(self.db.collection, self.db);
+  }
 
-	return self;
+  return self;
 };
 
 Database.prototype.getCollection = function(collectionName){
-	return wrapCollection(this.getCollectionBase(collectionName));
+  return wrapCollection(this.getCollectionBase(collectionName));
 };
 
 module.exports = new Database();
