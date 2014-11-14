0e48fb2606b4cdc940999fa007d03b082678f92c
alisamfp <eatliightning@gmail.com>
2014-11-14 14:12:09 -0700

    add support for object parameters

--- public/angular/controllers/param-input-controller.js
+++ public/angular/controllers/param-input-controller.js
@@ -1,39 +1,57 @@
 angular.module('octobluApp')
-.controller('ParamInputController', function($scope) {
-  'use strict';
-  var copyParamsToNgModel, instantiateParams;
+  .controller('ParamInputController', function($scope) {
+    'use strict';
+    var copyParamsToNgModel, instantiateParams;
 
-  $scope.ngModel = $scope.ngModel || {};
+    $scope.ngModel = $scope.ngModel || {};
+    var defaultSchema = {
+      type: "object",
+      properties: {}
+    };
 
-  instantiateParams = function(){
-    if(!$scope.paramDefinitions) { return; }
-
-    $scope.params = {};
-
-    _.each($scope.paramDefinitions, function(paramDefinition){
-      var existingValue = $scope.ngModel[paramDefinition.name];
-      $scope.params[paramDefinition.name] = existingValue || paramDefinition.default || '';
-    });
-  };
-
-  copyParamsToNgModel = function(){
-    if(!$scope.params) { return; }
-
-    $scope.ngModel = _.pick($scope.params, function(value){
-      return value !== '';
-    });
-  };
-
-  $scope.inputType = function(type){
-    if(type === 'boolean'){
-      return 'checkbox';
+    function getSchema(name) {
+      return _.extend({
+        title: name
+      }, defaultSchema);
     }
-    if(type === 'number'){
-      return 'number';
-    }
-    return 'text';
-  };
 
-  $scope.$watch('paramDefinitions', instantiateParams);
-  $scope.$watch('params', copyParamsToNgModel, true);
-});
+    instantiateParams = function() {
+      if (!$scope.paramDefinitions) {
+        return;
+      }
+
+      $scope.params = {};
+
+      _.each($scope.paramDefinitions, function(paramDefinition) {
+        var existingValue = $scope.ngModel[paramDefinition.name];
+        $scope.params[paramDefinition.name] = existingValue || paramDefinition.default || '';
+        
+        if(paramDefinition.type === 'object') {
+            paramDefinition.schema = getSchema(paramDefinition.name);
+        }
+      });
+    };
+
+    copyParamsToNgModel = function() {
+      if (!$scope.params) {
+        return;
+      }
+
+      $scope.ngModel = _.pick($scope.params, function(value) {
+        return value !== '';
+      });
+    };
+
+    $scope.inputType = function(type) {
+      if (type === 'boolean') {
+        return 'checkbox';
+      }
+      if (type === 'number') {
+        return 'number';
+      }
+      return 'text';
+    };
+
+    $scope.$watch('paramDefinitions', instantiateParams);
+    $scope.$watch('params', copyParamsToNgModel, true);
+  });
