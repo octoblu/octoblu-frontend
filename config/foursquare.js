4e4ba9e11639bfbcec07be1fc6d68f248caed071
redaphid <iam@aaronherres.com>
2014-11-13 18:05:48 -0700

    fixed foursqare stuff. probably.

--- config/foursquare.js
+++ config/foursquare.js
@@ -3,13 +3,13 @@ var FourSquare = require('passport-foursquare').Strategy;
 var User       = require('../app/models/user');
 var Channel    = require('../app/models/channel');
 
-var CONFIG = Channel.syncFindByType('channel:four-square').oauth[process.env.NODE_ENV];
+var CONFIG = Channel.syncFindByType('channel:foursquare').oauth[process.env.NODE_ENV];
 
 CONFIG.passReqToCallback = true;
 
 var fourSquareStrategy = new FourSquare(CONFIG, function(req, accessToken, refreshToken, profile, done){
 
-  User.addApiAuthorization(req.user, 'channel:four-square', {authtype: 'oauth', token: accessToken}).then(function () {
+  User.addApiAuthorization(req.user, 'channel:foursquare', {authtype: 'oauth', token: accessToken}).then(function () {
     done(null, req.user);
   }).catch(function(error){
     done(error);
