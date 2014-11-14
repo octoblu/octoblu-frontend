757402c185968e814c736d1d159683431be486d3
redaphid <iam@aaronherres.com>
2014-11-13 17:22:47 -0700

    fixed transposed redirect urls in twitter.json

--- public/angular/controllers/channel.js
+++ public/angular/controllers/channel.js
@@ -3,5 +3,4 @@ angular.module('octobluApp')
     .controller('ChannelController',function($scope, activeChannels,  availableChannels ) {
         $scope.activeChannels = activeChannels;
         $scope.availableChannels = availableChannels;
-
     });
