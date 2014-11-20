f72e564090759fa151825827c17fc77fb91f17f3
Jade Meskill <jade.meskill@gmail.com>
2014-11-20 15:23:10 -0700

    really fix it

--- public/angular/directives/flow/flow-renderer.js
+++ public/angular/directives/flow/flow-renderer.js
@@ -22,6 +22,8 @@ angular.module('octobluApp')
           return;
         }
         d3.event.preventDefault();
+        d3.event.stopPropagation();
+        dispatch.nodeSelected(node);
       };
       nodeElement.on('click', nodeClicked);
       nodeElement.on('dblclick', function(){
@@ -35,7 +37,6 @@ angular.module('octobluApp')
         var buttonClicked = function () {
           d3.event.preventDefault();
           d3.event.stopPropagation();
-          dispatch.nodeSelected(node);
           dispatch.nodeButtonClicked(node);
         };
         nodeElement.on('click', buttonClicked);
