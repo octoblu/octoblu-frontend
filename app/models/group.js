bfa0ce369294be5acfa9406d5269d78a408a9e5e
redaphid <iam@aaronherres.com>
2014-11-20 11:17:14 -0700

    single group->plural groups

--- app/models/group.js
+++ app/models/group.js
@@ -5,7 +5,7 @@ var when      = require('when');
 var uuid      = require('node-uuid');
 
 function GroupModel() {
-  var collection = octobluDB.getCollection('group');
+  var collection = octobluDB.getCollection('groups');
 
   var methods = {
     updateProperties : ['name', 'members'],
