0d99e042b8974d5049aca63e87fab48c9f8c69ae
Peter DeMartini and Roy van de Water <dev+peter+roy@integrumtech.com>
2014-12-09 14:10:25 -0700

    Moon island fixes for nedb and local meshblu

--- config/database.js
+++ config/database.js
@@ -8,7 +8,6 @@ var config = {
         databaseDirectory : path.join(__dirname, '../database'),
         url : process.env.OCTOBLU_DB || 'mongodb://localhost:27017/meshines',
         mongojsUrl : process.env.OCTOBLU_DB || 'mongodb://localhost:27017/meshines',
-        skynetUrl: process.env.SKYNET_DB || 'mongodb://localhost:27017/skynet',
         redisSessionUrl: process.env.REDIS_SESSION_URI || 'redis://localhost'
     },
     test: {
@@ -17,7 +16,6 @@ var config = {
         databaseType : 'mongodb',
         url: 'mongodb://localhost:27017/octoblu_test',
         mongojsUrl: 'mongodb://localhost:27017/octoblu_test',
-        skynetUrl: 'mongodb://localhost:27017/skynet-test',
         redisSessionUrl: 'redis://localhost/test-octoblu-session'
     },
     staging: {
@@ -26,7 +24,6 @@ var config = {
         databaseType : 'mongodb',
         url : 'mongodb://172.31.33.28:27017/octoblu-staging,mongodb://172.31.38.108:27017/octoblu-staging,mongodb://172.31.32.97:27017/octoblu-staging',
         mongojsUrl : '172.31.33.28,172.31.38.108,172.31.32.97/octoblu-staging?slaveOk=true',
-        skynetUrl : 'mongodb://172.31.33.28:27017/meshblu-staging,mongodb://172.31.38.108:27017/meshblu-staging,mongodb://172.31.32.97:27017/meshblu-staging',
         redisSessionUrl: 'redis://staging-redis.csy8op.0001.usw2.cache.amazonaws.com'
     },
     production: {
@@ -35,8 +32,13 @@ var config = {
         databaseType : 'mongodb',
         url : 'mongodb://172.31.33.28:27017/octoblu,mongodb://172.31.38.108:27017/octoblu,mongodb://172.31.32.97:27017/octoblu',
         mongojsUrl : '172.31.33.28,172.31.38.108,172.31.32.97/octoblu?slaveOk=true',
-        skynetUrl : 'mongodb://172.31.33.28:27017/meshblu,mongodb://172.31.38.108:27017/meshblu,mongodb://172.31.32.97:27017/meshblu',
         redisSessionUrl: 'redis://meshblu-redis.csy8op.0001.usw2.cache.amazonaws.com'
+    },
+    nedb: {
+        sessionSecret: 'e2em2miotskynetZOMGBBQ',
+        sessionDatabase : 'nedb', // nedb or nedb
+        databaseType: 'nedb', // nedb or nedb
+        databaseDirectory : path.join(__dirname, '../database')
     }
 };
 
