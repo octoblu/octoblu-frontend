ad5f090c6c90cf505c36fa9f4535a2e718236da9
alisamfp <eatliightning@gmail.com>
2014-12-30 09:37:43 -0700

    changed config

--- config/withings.js
+++ config/withings.js
@@ -1,17 +1,20 @@
 'use strict';
 var WithingsStrategy = require('passport-withings').Strategy;
-var User            = require('../app/models/user');
-var Channel         = require('../app/models/channel');
+var User = require('../app/models/user');
+var Channel = require('../app/models/channel');
 
 var CONFIG = Channel.syncFindOauthConfigByType('channel:withings');
 
 CONFIG.passReqToCallback = true;
 
-var withingsStrategy = new WithingsStrategy(CONFIG, function(req, accessToken, refreshToken, profile, done){
+var withingsStrategy = new WithingsStrategy(CONFIG, function(req, token, tokenSecret, profile, done) {
 
-  User.addApiAuthorization(req.user, 'channel:withings', {authtype: 'oauth', token: accessToken}).then(function () {
+  User.addApiAuthorization(req.user, 'channel:withings', {
+    authtype: 'oauth',
+    token: token
+  }).then(function() {
     done(null, req.user);
-  }).catch(function(error){
+  }).catch(function(error) {
     done(error);
   });
 });
