64f365e007194dc1e948c8f9af65d3f46e28e285
Peter DeMartini <sqrt+peter@octoblu.com>
2015-03-19 16:56:53 -0700

    Selfy not _thisy

--- app/controllers/flow-controller.js
+++ app/controllers/flow-controller.js
@@ -1,15 +1,15 @@
 var _ = require('lodash');
 
 module.exports = function (options) {
-  var _this, meshblu, Flow;
-  _this = this;
+  var self = this;
+  var meshblu, Flow;
 
   options = options || {};
 
   Flow    = options.Flow || require('../models/flow');
   meshblu = options.meshblu;
 
-  _this.create = function (req, res) {
+  self.create = function (req, res) {
     Flow.createByUserUUID(req.user.resource.uuid, req.body, meshblu).then(function(flow){
       res.send(201, flow);
     }, function(error) {
@@ -17,7 +17,7 @@ module.exports = function (options) {
     });
   };
 
-  _this.update = function (req, res) {
+  self.update = function (req, res) {
     Flow.updateByFlowIdAndUser(req.params.id, req.user.resource.uuid, req.body).then(function () {
       res.send(204);
     }, function (error) {
@@ -25,7 +25,7 @@ module.exports = function (options) {
     });
   };
 
-  _this.getAllFlows = function (req, res) {
+  self.getAllFlows = function (req, res) {
     return Flow.getFlows(req.user.resource.uuid).then(function(flows){
       res.send(flows);
     }, function(error){
@@ -33,7 +33,7 @@ module.exports = function (options) {
     });
   };
 
-  _this.delete = function (req, res) {
+  self.delete = function (req, res) {
     Flow.deleteByFlowIdAndUser(req.params.id, req.cookies.meshblu_auth_uuid, req.cookies.meshblu_auth_token, meshblu)
       .then(function(){
         res.send(204);
@@ -42,5 +42,5 @@ module.exports = function (options) {
       });
   };
 
-  return _this;
+  return self;
 };
