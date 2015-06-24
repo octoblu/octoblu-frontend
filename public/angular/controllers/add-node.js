178892a96e00b72ff7685d65ea8d143e9c907900
Square Root of Saturn <sqrt@octoblu.com>
2015-06-23 17:27:14 -0700

    added filtering to add node page

--- public/angular/controllers/add-node.js
+++ public/angular/controllers/add-node.js
@@ -1,8 +1,10 @@
 angular.module('octobluApp')
 .controller('addNodeController', function(OCTOBLU_API_URL, $scope, $state, NodeTypeService) {
   'use strict';
-
+  $scope.sortNodeMethod = 'name';
   NodeTypeService.getUnconfiguredNodeTypes().then(function(nodeTypes){
     $scope.nodeTypes = nodeTypes;
   });
+
+
 });
