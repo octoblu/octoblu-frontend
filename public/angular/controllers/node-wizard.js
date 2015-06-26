2c67084177d107b2745939be57413f15c0b0ea40
Sqrt of Octoblu <sqrt@octoblu.com>
2015-06-25 17:42:04 -0700

    updated the Nodes page. added blank state

--- public/angular/controllers/node-wizard.js
+++ public/angular/controllers/node-wizard.js
@@ -1,5 +1,50 @@
 'use strict';
 
 angular.module('octobluApp')
-.controller('nodeWizardController', function(OCTOBLU_API_URL, $scope) {
+.controller('nodeWizardController', function($scope, $state, NodeService, OCTOBLU_ICON_URL) {
+
+  NodeService.getNodes().then(function(nodes){
+    nodes = _.map(nodes, addLogoUrl);
+    $scope.loading = false;
+    $scope.configuredNodes = nodes;
+  });
+
+  function addLogoUrl(node){
+    if(node.logo){
+      return node;
+    }
+    if(node && node.type){
+      var type = node.type.replace('octoblu:', 'node:');
+      node.logo = OCTOBLU_ICON_URL + type.replace(':', '/') + '.svg';
+    } else {
+      node.logo = OCTOBLU_ICON_URL + 'node/other.svg';
+    }
+    return node;
+  }
+
+  $scope.nextStepUrl = function (node) {
+    var sref = 'material.' + node.category;
+    var params = {};
+    if (node.category === 'device' || node.category === 'microblu') {
+      params.uuid = node.uuid;
+    } else if (node.category === 'channel') {
+      params.id = node.channelid;
+    }
+    return $state.href(sref, params);
+  };
+
+  $scope.isAvailable = function (node) {
+    if (node.category === 'device' || node.category === 'microblu') {
+      return node.resource.online;
+    }
+    return true;
+  }
+
+  $scope.filterFlows = function(node) {
+    return node.type !== 'device:flow'
+  }
+
+  $scope.filterByDevicesAndMicroblu = function(node) {
+    return (node.category === 'device' || node.category === 'microblu');
+  }
 });
