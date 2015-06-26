b465365dad6b7eba0beba15d1d10b92554f13055
Sqrt of Octoblu <sqrt@octoblu.com>
2015-06-26 16:54:07 -0700

    Added Nodes Page

--- public/angular/controllers/node-wizard.js
+++ public/angular/controllers/node-wizard.js
@@ -2,6 +2,7 @@
 
 angular.module('octobluApp')
 .controller('nodeWizardController', function($scope, $state, NodeService, OCTOBLU_ICON_URL) {
+  console.log('Node Wizard Controller');
 
   NodeService.getNodes().then(function(nodes){
     nodes = _.map(nodes, addLogoUrl);
@@ -22,29 +23,4 @@ angular.module('octobluApp')
     return node;
   }
 
-  $scope.nextStepUrl = function (node) {
-    var sref = 'material.' + node.category;
-    var params = {};
-    if (node.category === 'device' || node.category === 'microblu') {
-      params.uuid = node.uuid;
-    } else if (node.category === 'channel') {
-      params.id = node.channelid;
-    }
-    return $state.href(sref, params);
-  };
-
-  $scope.isAvailable = function (node) {
-    if (node.category === 'device' || node.category === 'microblu') {
-      return node.resource.online;
-    }
-    return true;
-  }
-
-  $scope.filterFlows = function(node) {
-    return node.type !== 'device:flow'
-  }
-
-  $scope.filterByDevicesAndMicroblu = function(node) {
-    return (node.category === 'device' || node.category === 'microblu');
-  }
 });
