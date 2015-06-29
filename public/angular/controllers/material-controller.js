d82541000818d8420b54a5c34202483e3232cf45
Sqrt of Octoblu <sqrt@octoblu.com>
2015-06-29 13:45:21 -0700

    Reverted navbar controller

--- public/angular/controllers/material-controller.js
+++ public/angular/controllers/material-controller.js
@@ -3,7 +3,9 @@ angular.module('octobluApp')
   'use strict';
 
   $scope.isNavLockedOpen = function() {
-    return false;
+    if($state.is('material.design') || $state.is('material.flow')){
+      return false;
+    }
     return $mdMedia('gt-lg');
   };
 
