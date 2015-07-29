03dbe56be56361b588e3f0e0a19048539c6d4f12
Sqrt of Octoblu <sqrt@octoblu.com>
2015-07-28 17:41:55 -0700

    THE BUTTONS LOOK THE SAME YAY

--- public/angular/controllers/add-channel-noauth.js
+++ public/angular/controllers/add-channel-noauth.js
@@ -2,13 +2,12 @@
 
 angular.module('octobluApp')
 .controller('addChannelNoauthController', function ($scope, $state, nodeType, userService, channelService, AuthService) {
-  $scope.activate = function () {
-    AuthService.getCurrentUser().then(function(user){
-      userService.activateNoAuthChannel(user.resource.uuid, nodeType.channelid, function () {
-        channelService.getActiveChannels(true);
-        channelService.getAvailableChannels(true);
-        $state.go('material.design');
-      });
+  $scope.isLoading = true;
+  AuthService.getCurrentUser().then(function(user){
+    userService.activateNoAuthChannel(user.resource.uuid, nodeType.channelid, function () {
+      channelService.getActiveChannels(true);
+      channelService.getAvailableChannels(true);
+      $state.go('material.design');
     });
-  };
+  });
 });
