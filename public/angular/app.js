25753f0ac0d36d10df9166e55bd76796cd7fc39e
Sqrt of Saturn <sqrt@octoblu.com>
2015-08-21 16:03:26 -0700

    Too much login encoding

--- public/angular/app.js
+++ public/angular/app.js
@@ -89,11 +89,11 @@ angular.module('octobluApp', ['ngSanitize', 'ngCookies', 'ui.ace', 'ui.bootstrap
         responseError: function (response) {
           if (response.status === 401) {
             if($window.location.pathname !== '/login') {
-              return $window.location = '/login?callbackUrl=' + encodeURIComponent($location.url());
+              return $window.location = '/login?callbackUrl=' + $location.url();
             }
           }
           if (response.status === 403) {
-            return $window.location = '/profile/new?callbackUrl=' + encodeURIComponent($location.url());
+            return $window.location = '/profile/new?callbackUrl=' + $location.url();
           }
           if (response.status === 502) {
             if($window.location.pathname !== '/error') {
