3483c62d060655888e2ec100da20de81f592c135
sqrtofsaturn <sqrtofsaturn@gmail.com>
2015-09-24 15:41:50 -0700

    v1.5.95 more info when adding device to gateblu

--- public/angular/controllers/add-subdevice-form.js
+++ public/angular/controllers/add-subdevice-form.js
@@ -3,13 +3,25 @@ angular.module('octobluApp')
   'use strict';
 
   var gatebluLogger = new GatebluLogService();
+  var deviceToAdd = {};
 
   NodeTypeService.getNodeTypeById($stateParams.nodeTypeId).then(function(nodeType){
     $scope.nodeType = nodeType;
   }).then(function(){
+    $scope.registering = true;
+    $scope.updating = true;
+    $scope.loading = true;
     gatebluLogger.addDeviceBegin($stateParams.gatebluId, $scope.nodeType.connector);
     return GatebluService.addDevice($stateParams.gatebluId, $scope.nodeType, gatebluLogger);
+  }).then(function(results){
+    $scope.registering = false;
+    deviceToAdd = results[0];
+    return GatebluService.updateGateblu(results, gatebluLogger);
+  }).then(function(gateblu){
+    $scope.updating = false;
+    return GatebluService.waitForDeviceToHaveOptionsSchema(deviceToAdd, gatebluLogger);
   }).then(function(device){
+    $scope.loading = false
     gatebluLogger.addDeviceEnd(device.uuid, device.gateblu, device.connector);
     return ThingService.getThing({uuid: device.uuid});
   }).then(function(device){
@@ -18,7 +30,7 @@ angular.module('octobluApp')
 
   $scope.saveDevice = function(){
     return ThingService.updateDevice($scope.device).then(function(){
-      $state.go('material.design');
+      $state.go('material.configure');
     });
   };
 });
