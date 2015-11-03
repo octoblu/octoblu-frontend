1bdfcd32b05f2c20b303fc109cdaeea6688bcc3b
Sqrt of Octoblu <sqrt@octoblu.com>
2015-11-02 17:23:07 -0700

    added logoff event to cwc-navbar element when inserted into the dom

--- public/assets/javascripts/cwc-navbar-loader.js
+++ public/assets/javascripts/cwc-navbar-loader.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     var navbarElement = $("<cwc-navbar></cwc-navbar>")
     var navbarScriptElement = $("<script></script>");
     navbarElement.attr("domain", "https://console-eastus-bvtrelease-a.ctxwsdev.net");
+    navbarElement.attr("logoff-event", "$octobluUserLoggedOff"); 
     navbarScriptElement.attr("src", "https://console-eastus-bvtrelease-a.ctxwsdev.net/navbar/scripts/cwc-navbar.js");
     navbarScriptElement.attr("id", "cwc-navbar-source");
     var body = $('body');
