23416e8ea4314e45c736209faab3c2ce58a8d9ae
Sqrt of Octoblu <sqrt@octoblu.com>
2015-11-02 17:14:57 -0700

    Conditionally load CWC Navbar

---
+++ public/assets/javascripts/cwc-navbar-loader.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  if(window.location.hostname === "octoblu.cloud.com"){
+    var navbarElement = $("<cwc-navbar></cwc-navbar>")
+    var navbarScriptElement = $("<script></script>");
+    navbarElement.attr("domain", "https://console-eastus-bvtrelease-a.ctxwsdev.net");
+    navbarScriptElement.attr("src", "https://console-eastus-bvtrelease-a.ctxwsdev.net/navbar/scripts/cwc-navbar.js");
+    navbarScriptElement.attr("id", "cwc-navbar-source");
+    var body = $('body');
+    body.append(navbarScriptElement);
+    navbarElement.prependTo(body);
+  }
+});
