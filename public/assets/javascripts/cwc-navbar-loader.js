05c4aa61e1c79ef20236311551c61f740dad69d3
sqrtofsaturn <sqrtofsaturn@gmail.com>
2016-01-04 16:46:17 -0700

    Changed the navbar check to only load the navbar if there is a query param for referer that is for tryworkspaceservices

--- public/assets/javascripts/cwc-navbar-loader.js
+++ public/assets/javascripts/cwc-navbar-loader.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-  if(window.location.hostname === "octoblu.cloud.com"){
+  searchParams = window.location.searchParams;
+  if(searchParams.has("referer") && searchParams.get("referer") === "tryworkspaceservices"){
     var navbarElement = $("<cwc-navbar></cwc-navbar>")
     var navbarScriptElement = $("<script></script>");
     navbarElement.attr("domain", "https://console-eastus-bvtrelease-a.ctxwsdev.net");
-    navbarElement.attr("logoff-event", "$octobluUserLoggedOff"); 
+    navbarElement.attr("logoff-event", "$octobluUserLoggedOff");
     navbarScriptElement.attr("src", "https://console-eastus-bvtrelease-a.ctxwsdev.net/navbar/scripts/cwc-navbar.js");
     navbarScriptElement.attr("id", "cwc-navbar-source");
     var body = $('body');
