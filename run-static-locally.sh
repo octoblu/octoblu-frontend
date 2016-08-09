a55fdd25c429f5caf1f2ad0f8c85f8747e5846fa
Peter DeMartini <thepeterdemartini@gmail.com>
2016-08-08 17:30:02 -0700

    still not working

--- run-static-locally.sh
+++ run-static-locally.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker build -t local/app-octoblu .
-docker run --rm --name app-octoblu-local -p 3333:80 local/app-octoblu
+docker run --rm --name app-octoblu-local -p 3333:80 local/app-octoblu 
+
+# 'nginx-debug' '-g' 'daemon off;'
