e2717b1f19f47fb0061b242a4f5c5de3d355b785
Moheeb Zara <mzara1@asu.edu>
2016-09-09 13:49:56 -0700

    v8.6.7 null out responseText if using object

--- public/angular/controllers/echo-out-controller.js
+++ public/angular/controllers/echo-out-controller.js
@@ -1,8 +1,11 @@
 angular.module('octobluApp')
 .controller('EchoOutController', function($scope) {
+
   $scope.clearResponse = function() {
     if($scope.enable == true){
-      $scope.response = null
+      $scope.response = null;
+    }else if($scope.enable == false){
+      $scope.text = null;
     }
   };
 });
